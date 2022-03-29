bf2c0654f0f3a3848fabdf010c633584f1483070
Vadim A. Potemkin <aspirantvadim@gmail.com>
2022-03-29 13:30:39 +0300

    speed up results parsing

--- utils/results_parser.py
+++ utils/results_parser.py
@@ -1,17 +1,19 @@
 import os
-
+from utils import project_path
 import numpy as np
 import pandas as pd
 
 dataset_types = {
-    'equal': ['Trace', 'ShapesAll', 'Beef', 'DodgerLoopDay', 'ScreenType', 'Lightning7', 'EigenWorms', 'Rock',
+    'equal': ['Trace', 'ShapesAll', 'Beef', 'DodgerLoopDay', 'ScreenType', 'Lightning7', 'EigenWorms',
               'Plane', 'EthanolLevel', 'AsphaltRegulatiry', 'ElectricDevices', 'PowerCons', 'Herring'],
+
     'high_train': ['Worms', 'CounterMovementJump', 'FordA', 'DistalPhalanxOutlineCorrect', 'DistalPhalanxTW',
                    'FingerMovements', 'HandOutLines', 'FordB', 'SpokenArabicDigits', 'DistalPhalanxOutlineAgGroup',
                    'Tiselac', 'Earthquakes'],
+
     'low_train': ['Chinatown', 'ItalyPowerDemand', 'Crop', 'MoteStrain', 'DodgerLoopWeekend', 'ECG5000', 'ArrowHead',
                   'ChlorineConcentration', 'GunPoint', 'DodgerLoopGame', 'BME', 'DiatomSizeReduction', 'CinCECGTorso',
-                  'Haptics']
+                  'Haptics', 'Rock']
 }
 
 
@@ -19,17 +21,19 @@ class ResultsParser:
     """ Class for experiment results parsing """
 
     def __init__(self):
-        self.metrics = ['f1',
-                        'roc_auc',
-                        'accuracy',
-                        'logloss',
-                        'precision',
-                        ]
 
-        self.root_path = '../experiments/results_of_experiments/5_min'
-        self.comparision_path = '../experiments'
+        self.metrics_dict = {'f1': 'TESTF1',
+                             'roc_auc': 'TESTAUROC',
+                             'accuracy': 'TESTACC',
+                             'logloss': 'TESTNLL',
+                             'precision': 'TESTPrec'
+                             }
 
-        self.table = pd.DataFrame(columns=['dataset', 'run'] + self.metrics)
+        self.results_path = os.path.join(project_path(),
+                                         'results_of_experiments',
+                                         '5_min')
+        self.comparison_path = os.path.join(project_path(), 'results_of_experiments')
+        self.table = pd.DataFrame(columns=['dataset', 'run'] + list(self.metrics_dict.keys()))
         self.fill_table()
 
     def get_mean_pivot(self):
@@ -38,17 +42,16 @@ class ResultsParser:
 
     def fill_table(self):
         """ Function for parsing experiments results into single table """
-        if os.path.exists(self.root_path):
-            dataset_folders = [i.split('/')[-1] for i in self.list_dir(self.root_path)]
+        if os.path.exists(self.results_path):
+            dataset_folders = [i.split('/')[-1] for i in self.list_dir(self.results_path)]
             index = 0
             for dataset in dataset_folders:
-                for run in [i for i in self.list_dir(self.root_path + '/' + dataset) if len(i) < 3]:
+                for run in [i for i in self.list_dir(os.path.join(self.results_path, dataset)) if len(i) < 3]:
                     results = self.read_result(dataset, run)
                     if results is not None:
                         results = results.astype(float)
                     else:
                         continue
-
                     self.table.loc[index] = [dataset] + [run] + results.to_list()
                     index += 1
             return
@@ -57,13 +60,13 @@ class ResultsParser:
     def read_result(self, dataset, version):
         """ Function to parse a single result """
         try:
-            result = pd.read_csv(f'{self.root_path}/{dataset}/{version}/test_results/metrics.csv')['1']
+            result = pd.read_csv(f'{self.results_path}/{dataset}/{version}/test_results/metrics.csv')['1']
             return result
         except Exception:
             return None
 
     def save_to_csv(self, table, name):
-        table.to_csv(f'{self.root_path}/{name}.csv')
+        table.to_csv(f'{self.results_path}/{name}.csv')
 
     def read_mega_table(self, metric: str):
         """ Function for parsing table with results of experiments with different algorithms
@@ -71,20 +74,14 @@ class ResultsParser:
         :param metric: name of metric to extract from mega table (e.g. f1, roc_auc)
         :return specific (by metric) sheet of excel-table converted to pd.DataFrame
         """
-        if metric == 'roc_auc':
-            return pd.read_excel(self.comparision_path + '/MegaComparisonResultsSheet.xls', sheet_name='AUROCTest')
+        sheet_lists = {'roc_auc': 'AUROCTest',
+                       'f1': 'F1Test',
+                       'accuracy': 'ACCTest',
+                       'logloss': 'NLLTest',
+                       'precision': 'PrecTest'}
 
-        elif metric == 'f1':
-            return pd.read_excel(self.comparision_path + '/MegaComparisonResultsSheet.xls', sheet_name='F1Test')
-
-        elif metric == 'accuracy':
-            return pd.read_excel(self.comparision_path + '/MegaComparisonResultsSheet.xls', sheet_name='ACCTest')
-
-        elif metric == 'logloss':
-            return pd.read_excel(self.comparision_path + '/MegaComparisonResultsSheet.xls', sheet_name='NLLTest')
-
-        elif metric == 'precision':
-            return pd.read_excel(self.comparision_path + '/MegaComparisonResultsSheet.xls', sheet_name='PrecTest')
+        table_name = 'MegaComparisonResultsSheet.xls'
+        return pd.read_excel(os.path.join(self.comparison_path, table_name), sheet_name=sheet_lists[metric])
 
     def get_comparison(self, metric: str):
         """ Function for comparison FEDOT results with other algorithms by chosen metric
@@ -94,31 +91,22 @@ class ResultsParser:
         """
         mean_results = self.get_mean_pivot()
         results_mega_table = self.read_mega_table(metric)
-
-        help_dict = {'f1': 'TESTF1',
-                     'roc_auc': 'TESTAUROC',
-                     'accuracy': 'TESTACC',
-                     'logloss': 'TESTNLL',
-                     'precision': 'TESTPrec'
-                     }
+        metric_name_in_megatable = self.metrics_dict[metric]
 
         mega_table = results_mega_table.copy()
-        for name in ['fedot', 'outperformed_by_fedot', 'loose_percent']:
-            mega_table.insert(len(mega_table.loc[0]), name, np.NaN)
+        for new_col in ['fedot', 'outperformed_by_fedot', 'loose_percent']:
+            mega_table.insert(len(mega_table.loc[0]), new_col, np.NaN)
 
         for row in mean_results.iterrows():
-            name = row[0]
+            ds_name = row[0]
             metric_value = row[1][metric]
 
-            if name in mega_table[help_dict[metric]].to_list():
-
-                mega_table.loc[mega_table[help_dict[metric]] == name, ['fedot']] = metric_value
-
+            if ds_name in mega_table[metric_name_in_megatable].to_list():
+                mega_table.loc[mega_table[metric_name_in_megatable] == ds_name, ['fedot']] = metric_value
             else:
                 index = len(mega_table)
                 length = len(mega_table.loc[0])
-
-                mega_table.loc[index] = [name] + [np.NaN for _ in range(length - 4)] + [metric_value] + [np.NaN, np.NaN]
+                mega_table.loc[index] = [ds_name] + [np.NaN for _ in range(length - 4)] + [metric_value] + [np.NaN]*2
 
         # get comparison table where fedot is
         mega_table = mega_table[~mega_table['fedot'].isna()]
@@ -141,35 +129,31 @@ class ResultsParser:
             best_algos.append(best_algorithm)
             best_metrics.append(max_metric)
 
-            fedot_metric = mega_table.loc[mega_table[help_dict[metric]] == dataset_name]['fedot']
+            fedot_metric = float(mega_table.loc[mega_table[metric_name_in_megatable] == dataset_name]['fedot'])
             loose = abs(round(100 - fedot_metric * 100 / max_metric, 2))
-            mega_table.loc[mega_table[help_dict[metric]] == dataset_name, ['loose_percent']] = loose
+            mega_table.loc[mega_table[metric_name_in_megatable] == dataset_name, ['loose_percent']] = loose
 
-        # RANK CALCULATION
-        for index, row in mega_table.iterrows():
-            dataset_name = row[0]
             metrics = [value for value in mega_table.loc[index][1:15]]
-
-            fedot_metric = float(mega_table.loc[mega_table[help_dict[metric]] == dataset_name]['fedot'])
-
             lower_fedot = []
             for i in metrics:
                 if i <= fedot_metric or i == fedot_metric:
                     lower_fedot.append(i)
-
             rank = str(len(lower_fedot)) + '/' + str(len(metrics))
-            mega_table.loc[mega_table[help_dict[metric]] == dataset_name, ['outperformed_by_fedot']] = rank
+            mega_table.loc[mega_table[metric_name_in_megatable] == dataset_name, ['outperformed_by_fedot']] = rank
 
-        mega_table['dataset_types'] = np.NaN
+
+        dataset_type_list = []
         for row in mega_table.iterrows():
             dataset_name = row[1][0]
-            for dataset_type, value in dataset_types.items():
-                for ds in value:
-                    if ds == dataset_name:
-                        mega_table.loc[mega_table[help_dict[metric]] == dataset_name, ['dataset_types']] = dataset_type
+
+            types = dataset_types.keys()
+            for type in types:
+                if dataset_name in dataset_types[type]:
+                    dataset_type_list.append(type)
 
         mega_table['best_algo'] = best_algos
         mega_table['max_metric'] = best_metrics
+        mega_table['dataset_types'] = dataset_type_list
 
         return mega_table
 
@@ -189,13 +173,12 @@ class ResultsParser:
 
 # EXAMPLE
 
-ls = [
-    'f1',
-    # 'roc_auc', 'accuracy', 'logloss', 'precision'
-]
+ls = ['f1', 'roc_auc'
+      # , 'accuracy', 'logloss', 'precision'
+      ]
 c = ResultsParser()
 
 for metr in ls:
     table = c.get_comparison(metr)
 
-    c.save_to_csv(table, f'{metr}_mega_compare_20min')
+    c.save_to_csv(table, f'{metr}_mega_compare_5min')
