d20fe263165effcdb010f05fc621893cdbfc4eb1
Vadim A. Potemkin <aspirantvadim@gmail.com>
2022-04-12 13:56:19 +0300

    fixed dataset reading method

--- cases/run/utils.py
+++ cases/run/utils.py
@@ -1,20 +1,23 @@
 import pandas as pd
 import logging
+import os
+from core.operation.utils.utils import project_path
 
 
 def read_tsv(file_name: str):
     df_train = pd.read_csv(
-        r'D:\РАБОТЫ РЕПОЗИТОРИИ\Репозитории\IndustrialTS\data\{}\{}_TRAIN.tsv'.format(file_name, file_name).format(file_name, file_name),
-        # 'D:\РАБОТЫ РЕПОЗИТОРИИ\Репозитории\IndustrialTS\data\{}\{}_TRAIN.tsv'.format(file_name, file_name),
+        os.path.join(project_path(), 'data', file_name, f'{file_name}_TRAIN.tsv'),
         sep='\t',
         header=None)
+
     X_train = df_train.iloc[:, 1:]
     y_train = df_train[0].values
+
     df_test = pd.read_csv(
-        r'D:\РАБОТЫ РЕПОЗИТОРИИ\Репозитории\IndustrialTS\data\{}\{}_TEST.tsv'.format(file_name, file_name).format(file_name, file_name),
-        # 'D:\РАБОТЫ РЕПОЗИТОРИИ\Репозитории\IndustrialTS\data\{}\{}_TEST.tsv'.format(file_name, file_name),
+        os.path.join(project_path(), 'data', file_name, f'{file_name}_TEST.tsv'),
         sep='\t',
         header=None)
+
     X_test = df_test.iloc[:, 1:]
     y_test = df_test[0].values
 
