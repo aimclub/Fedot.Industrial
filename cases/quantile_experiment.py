1655e61b4c1958771ba03852695700c44f48d8e1
Vadim A. Potemkin <aspirantvadim@gmail.com>
2022-04-13 19:11:21 +0300

    first try

--- cases/quantile_experiment.py
+++ cases/quantile_experiment.py
@@ -3,35 +3,36 @@ from cases.run.utils import read_tsv
 
 if __name__ == '__main__':
     dict_of_dataset = {
-        'ElectricDevices': read_tsv('ElectricDevices'),
-        'Earthquakes': read_tsv('Earthquakes'),
-        'Beef': read_tsv('Beef'),
-        'Lightning7': read_tsv('Lightning7'),
+        # 'ElectricDevices': read_tsv('ElectricDevices'),
+        # 'Earthquakes': read_tsv('Earthquakes'),
+        # 'Beef': read_tsv('Beef'),
+        # 'Lightning7': read_tsv('Lightning7'),
+        'EthanolLevel': read_tsv('EthanolLevel')
     }
 
     dict_of_wavelet_list = {
         #'ElectricDevices': ['db5', 'sym5', 'coif5', 'bior2.4'],
-                            'Beef': ['db5', 'sym5', 'coif5', 'bior2.4']
+                            'EthanolLevel': ['db5', 'sym5', 'coif5', 'bior2.4']
                             }
 
     list_of_dataset = [
         #'ElectricDevices',
-                       'Beef',
+                       'EthanolLevel',
                        ]
 
     fedot_params = {'problem': 'classification',
                     'seed': 42,
-                    'timeout': 15,
+                    'timeout': 1,
                     'composer_params': {'max_depth': 10,
                                         'max_arity': 4,
                                         'cv_folds': 3,
                                         'stopping_after_n_generation': 20
                                         },
                     'verbose_level': 2,
-                    'n_jobs': 4}
+                    'n_jobs': -1}
 
     runner = StatsRunner(list_of_dataset,
-                         launches=3,
+                         launches=1,
                          fedot_params=fedot_params)
 
     models = runner.run_experiment(dict_of_dataset,
