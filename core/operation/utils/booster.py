7c54fb4ee31f1a8ee5f4c1217638ca513656a8e5
Vadim A. Potemkin <aspirantvadim@gmail.com>
2022-04-14 20:38:07 +0300

    first try

--- core/operation/utils/booster.py
+++ core/operation/utils/booster.py
@@ -1,107 +1,95 @@
-from fedot.api.main import Fedot
-from cases.analyzer import PerfomanceAnalyzer
-
-from cases.run.utils import read_tsv, get_logger
-from sklearn.metrics import roc_auc_score, f1_score
-from sklearn.model_selection import train_test_split
-from sklearn.utils import resample
-import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score
+from cases.analyzer import PerfomanceAnalyzer
+from cases.run.utils import get_logger
+from fedot.api.main import Fedot
 
-
-def get_score(target, prediction):
-    if len(np.unique(target)) > 2:
-        return roc_auc_score(target, prediction, multi_class='ovo')
-    return roc_auc_score(target, prediction)
+import numpy as np
+import timeit
 
 
 class Booster:
-
-    cycles = 0
+    CYCLES = 0
 
     def __init__(self,
-                 new_target,
                  previous_predict,
-                 input_data: tuple = None,  # TRAIN (X, y) subset of whole dataset
-                 metrics_name: list = ['f1', 'roc_auc', 'accuracy', 'logloss', 'precision'],
-                 # fedot_parameters: dict = None,
-                 # file_name: str = 'EthanolLevel',
+                 input_data: tuple = None,
+                 metrics_name: list = ('f1',
+                                       'roc_auc',
+                                       'accuracy',
+                                       'logloss',
+                                       'precision'),
                  ):
-        self.input_data = input_data
-        self.previous_predict = previous_predict
-        self.new_target = new_target
-        self.analyzer = PerfomanceAnalyzer()
+        self.previous_predict = previous_predict.reshape(previous_predict.shape[0],)
+
+        self.boost_predictions = pd.DataFrame({'0': self.previous_predict})
+
+        self.features = input_data[0]
+        self.target = input_data[1]
         self.metrics_name = metrics_name
 
-        # self.fedot_parameters = fedot_parameters
-        # self.file_name = file_name
-        # self.logger.info(f'Boosting of obtained model for <{self.file_name}> has started')
-        self.boost_predictions = {'0': self.previous_predict}
-
-        # self.features = 0
-        # self.target = 0
+        self.accuracy_base = accuracy_score(self.target, self.previous_predict)
+        self.f1_base = f1_score(self.target, self.previous_predict, average='weighted')
 
         self.logger = get_logger()
 
+    def get_sum(self):
+        columns = self.boost_predictions.columns
+        self.boost_predictions.columns['sum'] = 0
+        for col in columns:
+            self.boost_predictions.columns['sum'] += self.boost_predictions.columns[col]
+
+        return self.boost_predictions.columns['sum'].values
+
     def run_boosting(self):
-        # after every boost
-        self.cycles += 1
+        last_predict = self.previous_predict
+        self.logger.info('start cycle')
+        predictions, inference = self.run_cycle(last_predict)
+        predictions, inference = self.run_cycle(last_predict)
 
+        return predictions, inference
 
-    def get_boost_model(self):
-        self.cycles += 1
+    def run_cycle(self, last_predict):
+        self.CYCLES += 1
+        self.logger.info('-------calculate diff')
+        diff = self.target - last_predict
 
-        self.logger.info(f'Start cycle {self.cycles} of boosting')
+        predictor = Fedot(problem='regression',
+                          timeout=2,
+                          seed=20,
+                          verbose_level=1,
+                          n_jobs=-1)
+        X, y = self.features, self.target
 
-        fedot_model = Fedot(problem='classification',
-                            timeout=1,
-                            seed=20,
-                            verbose_level=2,
-                            n_jobs=-1)
-        self.logger.info('Reading dataset')
-        # data = pd.read_csv(r'C:\Users\User\Desktop\work-folder\industrial_ts\IndustrialTS\data\EthanolLevel\EthanolLevel_TRAIN.tsv',
-        #                        sep='\t',
-        #                        header=None)
-
-        # self.features = self.input_data[0]
-        # self.target = self.input_data[1]
-
-        X, y = self.input_data[0], self.input_data[1]
-
-        # Split Input data into train and test
+        self.logger.info('-------splitting set')
         X_train, _, y_train, _ = train_test_split(X,
-                                                  y,
+                                                  diff,
                                                   test_size=0.2,
                                                   random_state=np.random.randint(100))
-        pipeline = fedot_model.fit(X_train, y_train)
+        self.logger.info('-------fitting model')
 
-        # if len(np.unique(y_train)) > 2:
-        #     prediction = fedot_model.predict_proba(X_test)
-        # else:
+        predictor.fit(X_train, y_train)
+        self.logger.info('-------prediction model')
+        start_time = timeit.default_timer()
+        predictions = predictor.predict(X)
+        inference = timeit.default_timer() - start_time
+        # predictions_proba = predictor.predict_proba(self.features)
+        self.logger.info('-------filling dataframe')
+        self.boost_predictions[self.CYCLES] = predictions
+        self.logger.info('-------getting sum of predictions')
+        result_prediction = self.get_sum()
 
-        prediction = fedot_model.predict(X)
-        prediction_proba = fedot_model.predict_proba(X)
+        self.logger.info('-------calc accuracy model')
+        accuracy_before_boosting = accuracy_score(y, self.previous_predict)
+        accuracy_after_boosting = accuracy_score(y, result_prediction)
+        acc_boost = 100 * accuracy_after_boosting / accuracy_before_boosting
+        self.logger.info('-------calc F1 metric model')
+        f1_before_boosting = f1_score(y, self.previous_predict, average='weighted')
+        f1_after_boosting = f1_score(y, result_prediction, average='weighted')
+        f1_boost = 100 * f1_after_boosting / f1_before_boosting
 
-        corr = prediction.shape[0]
-        diff = y - prediction.reshape(corr, )
+        self.logger.info(f'{self.CYCLES} cycles of boosting increased accuracy to {acc_boost}%')
+        self.logger.info(f'{self.CYCLES} cycles of boosting increased F1 to {f1_boost}%')
 
-        self.boost_predictions[self.cycles] = prediction
-
-        sum_of_predictions = sum(self.boost_predictions.values())
-
-        try:
-            metrics = self.analyzer.calculate_metrics(self.metrics_name,
-                                                      target=y,
-                                                      predicted_labels=sum_of_predictions,
-                                                      predicted_probs=prediction_proba
-                                                      )
-        except Exception as ex:
-            metrics = 'empty'
-
-        print(f'Metrics with {self.cycles} of Boosting: {metrics}')
-
-#
-# if __name__ == '__main__':
-#     # data = read_tsv('EthanolLevel')
-#
-#     Booster().get_boost_model()
+        return predictions, inference
