b4b137c589a8c34eaa8abc84f9c655c6d044fb92
v1docq <ir@raxeltelematics.com>
2022-06-08 16:32:43 +0300

    architecture refactoring alpha stage

--- cases/run/ts_clf.py
+++ cases/run/ts_clf.py
@@ -1,7 +1,4 @@
-import json
-
 from fedot.api.main import Fedot
-
 from cases.analyzer import PerfomanceAnalyzer
 from core.operation.utils.Composer import FeatureGeneratorBuilder, FeatureGeneratorComposer
 
@@ -46,15 +43,20 @@ class TimeSeriesClf:
     def fit(self, train_tuple):
         feature_list = list(map(lambda x: x.extract_features(train_tuple[0]), self.list_of_generators))
         predictor_list = list(map(lambda x: self._fit_fedot_model(x, train_tuple[1]), feature_list))
-        return predictor_list
+        return dict(predictors=predictor_list, train_features=feature_list)
 
     def predict(self, predictor_list, test_tuple):
         feature_list = list(map(lambda x: x.extract_features(test_tuple[0]), self.list_of_generators))
-        predictions_list = list(map(lambda x, y: x.predict(y), predictor_list[0], feature_list))
-        predictions_proba_list = list(map(lambda x, y: x.predict_proba(y), predictor_list[0], feature_list))
-        metrics = list(map(lambda x, y: self.analyzer.calculate_metrics(self.metrics_name,
-                                                                        target=test_tuple[1],
-                                                                        predicted_labels=x,
-                                                                        predicted_probs=y
-                                                                        ), predictions_list, predictions_proba_list))
-        return predictions_list, predictions_proba_list, metrics
+        predictions_list = list(map(lambda x, y: x.predict(y), predictor_list, feature_list))
+        predictions_proba_list = list(map(lambda x, y: x.predict_proba(y), predictor_list, feature_list))
+        metrics_dict = list(map(lambda x, y: self.analyzer.calculate_metrics(self.metrics_name,
+                                                                             target=test_tuple[1],
+                                                                             predicted_labels=x,
+                                                                             predicted_probs=y
+                                                                             ), predictions_list,
+                                predictions_proba_list))
+
+        return dict(prediction=predictions_list,
+                    prediction_proba=predictions_proba_list,
+                    metrics=metrics_dict,
+                    test_features=feature_list)
