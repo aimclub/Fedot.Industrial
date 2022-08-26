761c81cfd6cb1b0ddeca067cdd95ccac21fc3369
vadim_potemkin <aspirantvadim@gmail.com>
2022-08-26 14:27:22 +0200

    improved areas detector

--- cases/anomaly_detection/clear_architecture/detectors/areas_detector.py
+++ cases/anomaly_detection/clear_architecture/detectors/areas_detector.py
@@ -1,9 +1,7 @@
 import numpy as np
 from sklearn import preprocessing
-from sklearn.metrics import f1_score
 
-from cases.anomaly_detection.clear_architecture.utils.get_time import get_current_time
-from cases.anomaly_detection.clear_architecture.utils.settings_args import SettingsArgs
+from cases.anomaly_detection.clear_architecture.detectors.AbstractDetector import AbstractDetector
 
 """
 input format:
@@ -14,8 +12,7 @@ Output
 """
 
 
-class AreasDetector:
-    args: SettingsArgs
+class AreasDetector(AbstractDetector):
 
     def __init__(self, quantile: float,
                  divider_for_anomaly_len_influence: float,
@@ -24,60 +21,9 @@ class AreasDetector:
         self.filtering = filtering
         self.divider = divider_for_anomaly_len_influence
 
-    def set_settings(self, args: SettingsArgs):
-        self.args = args
-        self.windowed_data: list = []
-        self.output_list: list = []
-        self._print_logs(f"{get_current_time()} Areas detector: settings was set.")
-        self._print_logs(f"{get_current_time()} Areas detector: Visualize = {self.args.visualize}")
-        self._print_logs(f"{get_current_time()} Areas detector: Print logs = {self.args.print_logs}")
+        super().__init__('Areas Detector')
 
-    def input_data(self, dictionary: dict) -> None:
-        self._print_logs(f"{get_current_time()} Areas detector: Data read!")
-        self.input_dict = dictionary
-        self.windowed_data = self.input_dict["data_body"]["windows_list"]
-        self.step = self.input_dict["data_body"]["window_step"]
-        self.len = self.input_dict["data_body"]["window_len"]
-        self.data = self.input_dict["data_body"]["elected_data"]
-        self.labels = self.input_dict["data_body"]["raw_labels"]
-        self.win_len = self.input_dict["data_body"]["window_len"]
-
-    def run(self) -> None:
-        self._print_logs(f"{get_current_time()} Areas detector: Start transforming...")
-        self._areas_analysis()
-        self._print_logs(f"{get_current_time()} Areas detector: Transforming finished!")
-
-    def output_data(self) -> dict:
-        if "detection" in self.input_dict["data_body"]:
-            previous_predict = self.input_dict["data_body"]["detection"]
-            """
-            for i, predict in enumerate(previous_predict):
-                for j in range(len(predict)):
-                    if predict[j] == 1:
-                        self.output_list[i][j] = 1
-            """
-            for i in range(len(self.output_list)):
-                self.output_list[i] = [self.output_list[i]]
-            for i in range(len(self.output_list)):
-                for j in range(len(previous_predict[i])):
-                    self.output_list[i].append(previous_predict[i][j])
-        else:
-            for i in range(len(self.output_list)):
-                self.output_list[i] = [self.output_list[i]]
-
-        self.input_dict["data_body"]["detection"] = self.output_list
-        if self.filtering:
-            score = []
-            for i in range(len(self.output_list)):
-                score.append(f1_score(self.labels[i], self.output_list[i], average='macro'))
-            print("-------------------------------------")
-            main_score = sum(score) / len(score)
-            print("Average predict:")
-            print(main_score)
-            print("-------------------------------------")
-        return self.input_dict
-
-    def _areas_analysis(self) -> None:
+    def _do_analysis(self):
 
         def get_distance(data: list, number: int) -> float:
             if data[0][number] >= data[1][number]:
@@ -134,11 +80,3 @@ class AreasDetector:
             reshaped_data = self.normalize_data(np.array(odd_new_predicts))
             self.output_list.append(reshaped_data.tolist())
             # self.output_list.append(odd_new_predicts)
-
-    @staticmethod
-    def normalize_data(data):
-        return (data - np.min(data)) / (np.max(data) - np.min(data))
-
-    def _print_logs(self, log_message: str) -> None:
-        if self.args.print_logs:
-            print(log_message)
