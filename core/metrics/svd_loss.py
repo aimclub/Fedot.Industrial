afc319d29ecb1b742bb9ad4e9202260a39a2371d
N-31V <litvintseva.ann@gmail.com>
2022-09-22 18:19:24 +0300

    Added factor into SVDLoss

--- core/metrics/svd_loss.py
+++ core/metrics/svd_loss.py
@@ -7,15 +7,16 @@ from torch.linalg import vector_norm, matrix_norm
 class SVDLoss(Module):
     """Base class for singular value decomposition losses."""
 
-    def __init__(self) -> None:
+    def __init__(self, factor: float = 1) -> None:
         super().__init__()
+        self.factor = factor
 
 
 class OrthogonalLoss(SVDLoss):
     """Calculates orthogonality loss for complex unitary matrices."""
 
-    def __init__(self, device: torch.device) -> None:
-        super().__init__()
+    def __init__(self, device: torch.device, factor: float = 1) -> None:
+        super().__init__(factor=factor)
         self.device = device
 
     def forward(self, model: Module) -> Tensor:
@@ -34,14 +35,14 @@ class OrthogonalLoss(SVDLoss):
                 r = Vh.size()[0]
                 E = torch.eye(r, device=self.device)
                 loss += matrix_norm(Vh @ Vh.transpose(0, 1) - E) ** 2 / r
-        return loss / n
+        return self.factor * loss / n
 
 
 class HoyerLoss(SVDLoss):
     """Calculates Hoyer Loss for diagonal matrix with singular values."""
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, factor: float = 1) -> None:
+        super().__init__(factor=factor)
 
     def forward(self, model: Module) -> Tensor:
         loss = 0
@@ -51,4 +52,4 @@ class HoyerLoss(SVDLoss):
                 n += 1
                 S = parameter
                 loss += vector_norm(S, ord=1) / vector_norm(S, ord=2)
-        return loss / n
+        return self.factor * loss / n
