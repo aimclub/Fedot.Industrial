8691a9d4c24068fa679d8ec647a3d55cc7b9df5f
vadim_potemkin <aspirantvadim@gmail.com>
2022-10-12 17:27:24 +0200

    optimized process flow for ts classification

--- core/TimeSeriesClassifier.py
+++ core/TimeSeriesClassifier.py
@@ -1,13 +1,13 @@
-from typing import Any, Dict, List, Union
+from typing import Union
 
 import numpy as np
 import pandas as pd
 from fedot.api.main import Fedot
-from pandas import DataFrame
+from numpy import ndarray
 
-from core.operation.utils.Composer import FeatureGeneratorComposer
+from core.models.ExperimentRunner import ExperimentRunner
+from core.operation.utils.analyzer import PerformanceAnalyzer
 from core.operation.utils.FeatureBuilder import FeatureBuilderSelector
-from core.operation.utils.TSDatatypes import FeatureList, MetricsDict, PredictionsList, PredictorList
 
 
 class TimeSeriesClassifier:
@@ -20,30 +20,35 @@ class TimeSeriesClassifier:
     """
 
     def __init__(self,
-                 feature_generator_dict: dict,
+                 # feature_generator_dict: dict,
+                 generator_name: str,
+                 generator_runner: ExperimentRunner,
                  model_hyperparams: dict,
                  ecm_model_flag: False):
         self.y_train = None
         self.predictor_list = None
         self.train_features = None
-        self.feature_generator_dict = feature_generator_dict
+        # self.feature_generator_dict = feature_generator_dict
+        self.generator_name = generator_name
+        self.generator_runner = generator_runner
+        self.feature_generator_dict = {self.generator_name: self.generator_runner}
         self.model_hyperparams = model_hyperparams
         self.ecm_model_flag = ecm_model_flag
-        self._init_composer()
+        # self._init_composer()
         self._init_builder()
 
-    def _init_composer(self) -> None:
-        """
-        Initialize composer with all operations.
-
-        :return: None
-        """
-        self.composer = FeatureGeneratorComposer()
-        if self.feature_generator_dict is not None:
-            for operation_name, operation_functionality in self.feature_generator_dict.items():
-                self.composer.add_operation(operation_name, operation_functionality)
-
-        self.list_of_generators = list(self.composer.dict.values())
+    # def _init_composer(self) -> None:
+    #     """
+    #     Initialize composer with all operations.
+    #
+    #     :return: None
+    #     """
+    #     self.composer = FeatureGeneratorComposer()
+    #     if self.feature_generator_dict is not None:
+    #         for operation_name, operation_functionality in self.feature_generator_dict.items():
+    #             self.composer.add_operation(operation_name, operation_functionality)
+    #
+    #     self.list_of_generators = list(self.composer.dict.values())
 
     def _init_builder(self) -> None:
         """
@@ -51,9 +56,8 @@ class TimeSeriesClassifier:
 
         :return: None
         """
-        for operation_name, operation_functionality in self.feature_generator_dict.items():
-            self.feature_generator_dict[operation_name] = \
-                FeatureBuilderSelector(operation_name, operation_functionality).select_transformation()
+        for name, runner in self.feature_generator_dict.items():
+            self.feature_generator_dict[name] = FeatureBuilderSelector(name, runner).select_transformation()
 
     def _fit_fedot_model(self, features: pd.DataFrame, target: np.ndarray) -> Fedot:
         """
@@ -68,41 +72,51 @@ class TimeSeriesClassifier:
 
     def fit(self, train_tuple: tuple, dataset_name: str) -> dict:
         self.y_train = train_tuple[1]
-        self.train_features = FeatureList(list_of_generators=self.list_of_generators,
-                                          data=train_tuple[0],
-                                          dataset_name=dataset_name).create()
+        # self.train_features = FeatureList(list_of_generators=self.list_of_generators,
+        #                                   data=train_tuple[0],
+        #                                   dataset_name=dataset_name).create()
 
-        self.predictor_list = PredictorList(train_labels_set=train_tuple[1],
-                                            feature_list=self.train_features,
-                                            operation=self._fit_fedot_model).create()
+        self.train_features = self.generator_runner.extract_features(train_tuple[0], dataset_name)
 
-        return dict(predictors=self.predictor_list,
-                    train_features=self.train_features)
+        # self.predictor_list = PredictorList(train_labels_set=train_tuple[1],
+        #                                     feature_list=self.train_features,
+        #                                     operation=self._fit_fedot_model).create()
 
-    def predict(self, predictor_list, test_tuple, dataset_name) -> List[Dict[str, Union[DataFrame, Any]]]:
+        self.predictor = self._fit_fedot_model(self.train_features, train_tuple[1])
 
-        feature_list = FeatureList(list_of_generators=self.list_of_generators,
-                                   data=test_tuple[0],
-                                   dataset_name=dataset_name).create()
+        return dict(predictor=self.predictor, train_features=self.train_features)
 
-        predictions_list = PredictionsList(predictor_list=predictor_list,
-                                           feature_list=feature_list,
-                                           operation='predictions').create()
+    def predict(self, predictor, test_tuple, dataset_name) -> dict:
+        # feature_list = FeatureList(list_of_generators=self.list_of_generators,
+        #                            data=test_tuple[0],
+        #                            dataset_name=dataset_name).create()
+        feature_list = self.generator_runner.extract_features(test_tuple[0], dataset_name)
 
-        predictions_proba_list = PredictionsList(predictor_list=predictor_list,
-                                                 feature_list=feature_list,
-                                                 operation='predictions_proba').create()
+        # predictions_list = PredictionsList(predictor_list=predictor_list,
+        #                                    feature_list=feature_list,
+        #                                    operation='predictions').create()
 
-        metrics_dict = MetricsDict(predictions_list, predictions_proba_list, test_tuple[1]).create()
+        prediction_label = predictor.predict(feature_list)
+        prediction_proba = predictor.predict_proba(feature_list)
 
-        return [dict(prediction=predictions_list[i],
-                     predictions_proba=predictions_proba_list[i],
-                     metrics=metrics_dict[i],
-                     test_features=feature_list[i]) for i in range(len(predictions_list))]
+        # predictions_proba_list = PredictionsList(predictor_list=predictor_list,
+        #                                          feature_list=feature_list,
+        #                                          operation='predictions_proba').create()
 
-    def predict_on_train(self) -> List[Dict[str, Union[DataFrame, Any]]]:
-        predictions_proba_list_train = PredictionsList(predictor_list=self.predictor_list,
-                                                       feature_list=self.train_features,
-                                                       operation='predictions_proba').create()
+        metrics_dict = PerformanceAnalyzer().calculate_metrics(target=test_tuple[1],
+                                                               predicted_labels=prediction_label,
+                                                               predicted_probs=prediction_proba)
 
-        return predictions_proba_list_train
+        # metrics_dict = MetricsDict(predictions_list, predictions_proba_list, test_tuple[1]).create()
+
+        return dict(prediction=prediction_label, predictions_proba=prediction_proba,
+                    metrics=metrics_dict, test_features=feature_list)
+
+    def predict_on_train(self) -> Union[ndarray, ndarray]:
+        prediction_proba = self.predictor.predict_proba(self.train_features)
+
+        # predictions_proba_list_train = PredictionsList(predictor_list=self.predictor_list,
+        #                                                feature_list=self.train_features,
+        #                                                operation='predictions_proba').create()
+
+        return prediction_proba
--- core/api/tsc_API.py
+++ core/api/tsc_API.py
@@ -5,6 +5,7 @@ from typing import Any, Union
 import numpy as np
 import pandas as pd
 import yaml
+from fedot.api.main import Fedot
 
 from core.models.ecm.ErrorRunner import ErrorCorrectionModel
 from core.models.EnsembleRunner import EnsembleRunner
@@ -125,15 +126,15 @@ class Industrial:
         """
         self.logger.info(f'START EXPERIMENT')
         experiment_dict = self._init_experiment_setup(config_name)
-
-        classificator = TimeSeriesClassifier(feature_generator_dict=experiment_dict['feature_generator'],
-                                             model_hyperparams=experiment_dict['fedot_params'],
-                                             ecm_model_flag=experiment_dict['error_correction'])
         launches = self.config_dict['launches']
+
+        # classificator = TimeSeriesClassifier(feature_generator_dict=experiment_dict['feature_generator'],
+        #                                      model_hyperparams=experiment_dict['fedot_params'],
+        #                                      ecm_model_flag=experiment_dict['error_correction'])
 
         for dataset_name in self.config_dict['datasets_list']:
             self.logger.info(f'START WORKING on {dataset_name} dataset')
-            try: # to load data
+            try:  # to load data
                 train_data, test_data = DataLoader(dataset_name).load_data()
                 self.logger.info(f'Loaded data from {dataset_name} dataset')
             except Exception:
@@ -143,75 +144,73 @@ class Industrial:
             n_classes = len(np.unique(train_data[1]))
             self.logger.info(f'{n_classes} classes detected')
 
-            classificator.model_hyperparams['metric'] = self._check_metric(n_classes)
+            for runner_name, runner in experiment_dict['feature_generator'].items():
+                classificator = TimeSeriesClassifier(generator_name=runner_name,
+                                                     generator_runner=runner,
+                                                     model_hyperparams=experiment_dict['fedot_params'],
+                                                     ecm_model_flag=experiment_dict['error_correction'])
+                classificator.model_hyperparams['metric'] = self._check_metric(n_classes)
 
-            for launch in range(1, launches + 1):
-                self.logger.info(f'START LAUNCH {launch}')
-                paths_to_save = list(map(lambda x: os.path.join(path_to_save_results(), x, dataset_name, str(launch)),
-                                         list(experiment_dict['feature_generator'].keys())))
+                for launch in range(1, launches + 1):
+                    self.logger.info(f'START LAUNCH {launch}')
+                    paths_to_save = os.path.join(path_to_save_results(), runner_name, dataset_name, str(launch))
 
-                self.logger.info('START TRAINING')
-                fitted_results = list(map(lambda x: classificator.fit(x, dataset_name), [train_data]))
-                fitted_predictor = fitted_results[0]['predictors']
-                train_features = fitted_results[0]['train_features']
+                    self.logger.info('START TRAINING')
+                    fitted_results = classificator.fit(train_data, dataset_name)
+                    fitted_predictor = fitted_results['predictor']
+                    train_features = fitted_results['train_features']
 
-                self.logger.info('START PREDICTION')
-                predictions = classificator.predict(fitted_predictor, test_data, dataset_name)
-                predict_on_train = classificator.predict_on_train()
+                    self.logger.info('START PREDICTION')
+                    predictions = classificator.predict(fitted_predictor,
+                                                        test_data,
+                                                        dataset_name)
 
-                ecm_results = [[]] * len(paths_to_save)
-                if self.config_dict['error_correction']:
-                    ecm_fedot_params = dict(problem='regression',
-                                            seed=14,
-                                            timeout=1,
-                                            max_depth=10,
-                                            max_arity=4,
-                                            cv_folds=2,
-                                            logging_level=20,
-                                            n_jobs=4)
+                    if self.config_dict['error_correction']:
+                        predict_on_train = classificator.predict_on_train()
+                        ecm_fedot_params = experiment_dict['fedot_params']
+                        ecm_fedot_params['problem'] = 'regression'
+                        ecm_fedot_params['timeout'] = round(experiment_dict['fedot_params']['timeout'] / 3)
 
-                    ecm_params = dict(n_classes=n_classes,
-                                      dataset_name=dataset_name,
-                                      save_models=False,
-                                      fedot_params=ecm_fedot_params)
+                        ecm_params = dict(n_classes=n_classes,
+                                          dataset_name=dataset_name,
+                                          save_models=False,
+                                          fedot_params=ecm_fedot_params)
 
-                    try:
-                        self.logger.info('START COMPOSE ECM')
-                        ecm_results = list(map(lambda x, y, z, m: ErrorCorrectionModel(**ecm_params,
-                                                                                       results_on_test=x,
-                                                                                       results_on_train=y,
-                                                                                       train_data=(z, train_data[1]),
-                                                                                       test_data=(m, test_data[1])).run(),
-                                               predictions,
-                                               predict_on_train,
-                                               train_features,
-                                               predictions))
-                    except Exception:
-                        self.logger.info('ECM COMPOSE WAS FAILED')
+                        try:
+                            self.logger.info('START COMPOSE ECM')
+                            ecm_results = ErrorCorrectionModel(**ecm_params,
+                                                               results_on_test=predictions,
+                                                               results_on_train=predict_on_train,
+                                                               train_data=train_data,
+                                                               test_data=test_data,
+                                                               ).run()
+                        except Exception:
+                            self.logger.info('ECM COMPOSE WAS FAILED')
+                    else:
+                        ecm_results = None
 
-                self.logger.info('SAVING RESULTS')
+                    self.logger.info('SAVING RESULTS')
 
-                for i in range(len(paths_to_save)):
                     self.save_results(train_target=train_data[1],
                                       test_target=test_data[1],
-                                      path_to_save=paths_to_save[i],
-                                      train_features=train_features[i],
-                                      prediction=predictions[i],
+                                      path_to_save=paths_to_save,
+                                      train_features=train_features,
+                                      prediction=predictions,
                                       fitted_predictor=fitted_predictor,
-                                      ecm_results=ecm_results[i])
+                                      ecm_results=ecm_results),
 
-                # spectral_generators = [x for x in paths_to_save if 'spectral' in x]
-                # if len(spectral_generators) != 0:
-                #     self._save_spectrum(classificator, path_to_save=spectral_generators)
+                    # spectral_generators = [x for x in paths_to_save if 'spectral' in x]
+                    # if len(spectral_generators) != 0:
+                    #     self._save_spectrum(classificator, path_to_save=spectral_generators)
 
         self.logger.info('END OF EXPERIMENT')
 
     def save_results(self, train_target: Union[np.ndarray, pd.Series],
                      test_target: Union[np.ndarray, pd.Series],
                      path_to_save: str,
-                     prediction: Any,
+                     prediction: dict,
                      train_features: Union[np.ndarray, pd.DataFrame],
-                     fitted_predictor,
+                     fitted_predictor: Fedot,
                      ecm_results: dict):
 
         metrics, predictions = prediction['metrics'], prediction['prediction']
@@ -221,17 +220,19 @@ class Industrial:
         os.makedirs(path_results, exist_ok=True)
 
         try:
-            for predictor in fitted_predictor:
-                predictor.current_pipeline.save(path_results)
+            fitted_predictor.current_pipeline.save(path_results)
         except Exception as ex:
             self.logger.error(f'Can not save pipeline: {ex}')
 
-        if len(ecm_results) != 0:
+        if ecm_results:
             self.save_boosting_results(**ecm_results, path_to_save=path_results)
 
         features_names = ['train_features.csv', 'train_target.csv', 'test_features.csv', 'test_target.csv']
         features_list = [train_features, train_target, test_features, test_target]
-        _ = list(map(lambda x, y: pd.DataFrame(x).to_csv(os.path.join(path_to_save, y)), features_list, features_names))
+
+        for name, features in zip(features_names, features_list):
+            pd.DataFrame(features).to_csv(os.path.join(path_to_save, name))
+
 
         if type(predictions_proba) is not pd.DataFrame:
             df_preds = pd.DataFrame(predictions_proba)
