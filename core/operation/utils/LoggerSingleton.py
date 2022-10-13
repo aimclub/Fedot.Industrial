51b200197a132f1a1bef11b7e69b6b3cc250ff9f
vadim_potemkin <aspirantvadim@gmail.com>
2022-10-13 15:51:08 +0200

    refactored process flow for TS classification; fixed logged double message issue

--- core/operation/utils/LoggerSingleton.py
+++ core/operation/utils/LoggerSingleton.py
@@ -1,7 +1,6 @@
 import datetime
 import logging
 import os.path
-from logging.handlers import RotatingFileHandler
 
 from core.operation.utils.utils import PROJECT_PATH
 
@@ -29,50 +28,22 @@ class Logger(object, metaclass=SingletonMetaLogger):
         self._logger.setLevel(logging.INFO)
         self._logger.propagate = False
 
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s",
-                                      "%Y-%m-%d %H:%M:%S")
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # formatter = CustomFormatter()
         now = datetime.datetime.now()
         dirname = os.path.join(PROJECT_PATH, 'log')
 
         if not os.path.isdir(dirname):
             os.mkdir(dirname)
-        # file_handler = RotatingFileHandler(dirname + "/log_" + now.strftime("%Y-%m-%d-%H:%M") + ".log",
-        #                                    maxBytes=100000000,
-        #                                    backupCount=1)
-        file_handler = logging.FileHandler(dirname + "/log_" + now.strftime("%Y-%m-%d-%H:%M") + ".log",
-                                           delay=True,
-                                           mode='w')
+        fileHandler = logging.FileHandler(dirname + "/log_" + now.strftime("%Y-%m-%d")+".log")
 
-        stream_handler = logging.StreamHandler()
+        streamHandler = logging.StreamHandler()
 
-        file_handler.setFormatter(formatter)
-        stream_handler.setFormatter(formatter)
+        fileHandler.setFormatter(formatter)
+        streamHandler.setFormatter(formatter)
 
-        self._logger.addHandler(file_handler)
-        self._logger.addHandler(stream_handler)
+        self._logger.addHandler(fileHandler)
+        self._logger.addHandler(streamHandler)
 
     def get_logger(self):
         return self._logger
-
-
-# class CustomFormatter(logging.Formatter):
-#
-#     grey = "\x1b[38;20m"
-#     yellow = "\x1b"
-#     red = "\x1b[31;20m"
-#     bold_red = "\x1b[31;1m"
-#     reset = "\x1b[0m"
-#     format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-#
-#     FORMATS = {
-#         logging.DEBUG: grey + format + reset,
-#         logging.INFO: yellow + format,
-#         logging.WARNING: red + format + reset,
-#         logging.ERROR: red + format + reset,
-#         logging.CRITICAL: bold_red + format + reset
-#     }
-#
-#     def format(self, record):
-#         log_fmt = self.FORMATS.get(record.levelno)
-#         formatter = logging.Formatter(log_fmt)
-#         return formatter.format(record)
