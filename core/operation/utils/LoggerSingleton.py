f323308142d162019ece7048ef106920b7bbc086
vadim_potemkin <aspirantvadim@gmail.com>
2022-10-13 15:51:08 +0200

    refactored process flow for TS classification; fixed logged double message issue

--- core/operation/utils/LoggerSingleton.py
+++ core/operation/utils/LoggerSingleton.py
@@ -1,16 +1,8 @@
+import datetime
 import logging
 import os.path
-from datetime import date, datetime
 
 from core.operation.utils.utils import PROJECT_PATH
-
-MSG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-DT_FORMAT = '%H:%M:%S'
-DATE_NOW = date.today()
-TIME_NOW = datetime.now().strftime("%H-%M")
-
-if not os.path.exists(PROJECT_PATH + '/log'):
-    os.mkdir(PROJECT_PATH + '/log')
 
 
 class SingletonMetaLogger(type):
@@ -21,9 +13,7 @@ class SingletonMetaLogger(type):
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            log_path = os.path.join(PROJECT_PATH, 'log', f'Experiment-log-{DATE_NOW}_{TIME_NOW}.log')
-            instance = super().__call__(log_path)
-            cls._instances[cls] = instance
+            cls._instances[cls] = super(SingletonMetaLogger, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
 
@@ -31,20 +21,29 @@ class Logger(object, metaclass=SingletonMetaLogger):
     """
     Class for implementing singleton Logger
     """
+    _logger = None
 
-    def __init__(self, log_path):
-        # logging.basicConfig(filename=log_path)
+    def __init__(self):
         self._logger = logging.getLogger('FEDOT-TSC')
         self._logger.setLevel(logging.INFO)
         self._logger.propagate = False
 
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # formatter = CustomFormatter()
+        now = datetime.datetime.now()
+        dirname = os.path.join(PROJECT_PATH, 'log')
 
-        formatter = logging.Formatter(MSG_FORMAT, DT_FORMAT)
-        ch.setFormatter(formatter)
+        if not os.path.isdir(dirname):
+            os.mkdir(dirname)
+        fileHandler = logging.FileHandler(dirname + "/log_" + now.strftime("%Y-%m-%d")+".log")
 
-        self._logger.addHandler(ch)
+        streamHandler = logging.StreamHandler()
+
+        fileHandler.setFormatter(formatter)
+        streamHandler.setFormatter(formatter)
+
+        self._logger.addHandler(fileHandler)
+        self._logger.addHandler(streamHandler)
 
     def get_logger(self):
         return self._logger
