f0552aaccfde622274ec1886b99df9620c585150
vadim_potemkin <aspirantvadim@gmail.com>
2022-10-14 11:33:39 +0200

    hotfix of auto window selection issue

--- core/TimeSeriesClassifier.py
+++ core/TimeSeriesClassifier.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 import numpy as np
 import pandas as pd
 from fedot.api.main import Fedot
--- core/operation/utils/LoggerSingleton.py
+++ core/operation/utils/LoggerSingleton.py
@@ -53,3 +53,26 @@ class Logger(object, metaclass=SingletonMetaLogger):
 
     def get_logger(self):
         return self._logger
+
+
+# class CustomFormatter(logging.Formatter):
+#
+#     grey = "\x1b[38;20m"
+#     yellow = "\x1b"
+#     red = "\x1b[31;20m"
+#     bold_red = "\x1b[31;1m"
+#     reset = "\x1b[0m"
+#     format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+#
+#     FORMATS = {
+#         logging.DEBUG: grey + format + reset,
+#         logging.INFO: yellow + format,
+#         logging.WARNING: red + format + reset,
+#         logging.ERROR: red + format + reset,
+#         logging.CRITICAL: bold_red + format + reset
+#     }
+#
+#     def format(self, record):
+#         log_fmt = self.FORMATS.get(record.levelno)
+#         formatter = logging.Formatter(log_fmt)
+#         return formatter.format(record)
