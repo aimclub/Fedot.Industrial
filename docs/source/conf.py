bb0f365d08e75e08c7907ec1d2b628288ab22441
vadim_potemkin <aspirantvadim@gmail.com>
2022-11-24 17:11:42 +0100

    small style fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,14 +70,6 @@ html_theme_options = {
     'logo_only': True,
     'display_version': False,
 }
-html_static_path = ['_static']
-
-
-
-
-def setup(app):
-    app.add_css_file('css/custom.css')
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
