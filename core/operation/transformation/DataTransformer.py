046698f8f37eb868daac97a451c3f13450c980a9
N-31V <litvintseva.ann@gmail.com>
2022-12-14 15:43:57 +0300

    Dataset classes moved to datasets folder.

--- core/operation/transformation/DataTransformer.py
+++ core/operation/transformation/DataTransformer.py
@@ -1,47 +1,10 @@
-from typing import Union, Tuple
+from typing import Union
 
 import numpy as np
 import pandas as pd
+from ripser import Rips, ripser
 from scipy import sparse
 from scipy.spatial.distance import pdist, squareform
-
-from ripser import Rips, ripser
-
-import torch
-from torch.utils.data import Dataset
-
-
-class CustomClassificationDataset(Dataset):
-    """Class for custom classification datasets.
-
-    Args:
-        images: Numpy matrix of images.
-        targets: Numpy vector of targets.
-    """
-
-    def __init__(
-            self,
-            images: np.ndarray,
-            targets: np.ndarray,
-    ) -> None:
-        self.images = torch.from_numpy(images).float()
-        self.targets = torch.from_numpy(targets).type(torch.long)
-
-    def __getitem__(self, idx) -> Tuple[torch.Tensor, int]:
-        """Returns a sample from a dataset.
-
-        Args:
-            idx: Index of sample.
-
-        Returns:
-            A tuple ``(image, target)``, where image is image tensor,
-                and target is integer.
-        """
-        return self.images[idx], self.targets[idx]
-
-    def __len__(self) -> int:
-        """Return length of dataset"""
-        return self.targets.size()[0]
 
 
 class TopologicalTransformation:
