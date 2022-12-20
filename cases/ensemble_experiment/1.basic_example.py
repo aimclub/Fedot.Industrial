e285ae9c4485b5be10e67adc311e3aa0ea2f8205
v1docq <ir@raxeltelematics.com>
2022-12-20 18:26:40 +0300

    API refactoring 0.4.1 fixes

--- cases/ensemble_experiment/1.basic_example.py
+++
@@ -1,6 +0,0 @@
-from core.api.API import Industrial
-
-if __name__ == '__main__':
-    config_name = 'cases/config/configs_for_examples/BasicConfigEnsemble.yaml'
-    ExperimentHelper = Industrial()
-    ExperimentHelper.run_experiment(config_name, direct_path=False)
