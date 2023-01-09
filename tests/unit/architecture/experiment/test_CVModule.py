4c123551463e0d8ca6c7d1429489606dfb8866d0
N-31V <litvintseva.ann@gmail.com>
2023-01-09 13:54:08 +0300

    fix import, removed initialization of default parameters, turned on one detection test

--- tests/unit/architecture/experiment/test_CVModule.py
+++ tests/unit/architecture/experiment/test_CVModule.py
@@ -1,25 +1,20 @@
 import os
 
 import pytest
-import torch
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import Compose, ToTensor, Resize
 
-from core.architecture.datasets.object_detection_datasets import COCODataset, collate_fn
+from core.architecture.datasets.object_detection_datasets import COCODataset
 from core.architecture.experiment.CVModule import ClassificationExperimenter, \
     FasterRCNNExperimenter
 from core.architecture.utils.utils import PROJECT_PATH
 
 SVD_PARAMS = {
-    'orthogonal_loss_factor': 10,
-    'hoer_loss_factor': 0.001,
     'energy_thresholds': [0.9],
-    'finetuning_epochs': 1
 }
 
 SFP_PARAMS = {
     'pruning_ratio': 0.5,
-    'finetuning_epochs': 1
 }
 
 DATASETS_PATH = os.path.join(PROJECT_PATH, 'tests/data/datasets/')
@@ -37,15 +32,8 @@ def get_test_classification_dataset(tmp_path):
         'num_classes': 3,
         'dataloader_params': {'batch_size': 16, 'num_workers': 4},
         'model': 'ResNet18',
-        'model_params': {},
         'models_saving_path': tmp_path.joinpath('models'),
-        'optimizer': torch.optim.Adam,
-        'optimizer_params': {},
-        'target_loss': torch.nn.CrossEntropyLoss,
-        'loss_params': {},
-        'metric': 'f1',
         'summary_path': tmp_path.joinpath('runs'),
-        'summary_per_class': True,
         'gpu': False
     }
     yield exp_params, tmp_path
@@ -149,22 +137,15 @@ def get_test_detection_dataset(tmp_path):
         'train_dataset': dataset,
         'val_dataset': dataset,
         'num_classes': len(dataset.classes) + 1,
-        'dataloader_params': {'batch_size': 1, 'num_workers': 2,
-                              'collate_fn': collate_fn},
-        # 'model_params': {'weights_backbone': None},
+        'dataloader_params': {'batch_size': 1, 'num_workers': 2},
         'model_params': {'weights': 'DEFAULT'},
         'models_saving_path': tmp_path.joinpath('models'),
-        'optimizer': torch.optim.SGD,
-        'optimizer_params': {'lr': 0.0001, 'momentum': 0.9, 'weight_decay': 0.0005},
-        'scheduler_params': {'step_size': 5, 'gamma': 0.5},
-        'metric': 'map',
         'summary_path': tmp_path.joinpath('runs'),
-        'summary_per_class': False,
         'gpu': False
     }
     yield exp_params, tmp_path
 
-@pytest.mark.skip(reason='still in development')
+
 def detection_predict(experimenter):
     test_predict_path = DATASETS_PATH + 'ALET10/test'
     preds = experimenter.predict(test_predict_path)
@@ -176,7 +157,7 @@ def detection_predict(experimenter):
     for k, v in proba_preds.items():
         assert set(v.keys()) == {'labels', 'boxes', 'scores'}
 
-@pytest.mark.skip(reason='still in development')
+
 def test_fasterrcnn_experimenter(get_test_detection_dataset):
     exp_params, tmp_path = get_test_detection_dataset
     experimenter = FasterRCNNExperimenter(
