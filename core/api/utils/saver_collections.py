e7e3ac82cece667fbeefafe57a9081290e6b3cbc
vadim_potemkin <aspirantvadim@gmail.com>
2023-01-30 12:02:47 +0100

    eliminated agg ensembler; refactored bench for TSC

--- core/api/utils/saver_collections.py
+++ core/api/utils/saver_collections.py
@@ -6,54 +6,61 @@ from fedot.core.log import default_log as logger
 
 class ResultSaver:
     def __init__(self):
-
+        self.path = None
         self.logger = logger(self.__class__.__name__)
         self.save_method_dict = {'ECM': self.save_boosting_results,
                                  'Ensemble': self.save_ensemble_results,
-                                 'Original': self.save_results}
+                                 'Original': self.save_basic_results}
 
-    def save_results(self,
-                     prediction: dict
-                     ):
-        for launch in prediction:
-            result_at_launch = prediction[launch]
-            feature_target_dict = {'train_features.csv': result_at_launch['train_features'],
-                                   'train_target.csv': result_at_launch['train_target'],
-                                   'test_features.csv': result_at_launch['test_features'],
-                                   'test_target.csv': result_at_launch['test_target']}
-            path_results = os.path.join(result_at_launch['path_to_save'], 'test_results')
-            os.makedirs(path_results, exist_ok=True)
+    def save_basic_results(self,
+                           prediction: dict, path, dataset_name: str):
+        for model in prediction:
+            if prediction[model]:
+                path_to_save = os.path.join(path, model, dataset_name)
+                for launch in prediction[model]:
+                    if prediction[model][launch]:
+                        path_test_results = os.path.join(path_to_save, str(launch), 'test_results')
+                        path_features = os.path.join(path_to_save, str(launch))
+                        path_pipeline = os.path.join(path_to_save, str(launch), 'tsc_pipeline')
+                        os.makedirs(path_test_results, exist_ok=True)
 
-            try:
-                result_at_launch['fitted_predictor'].current_pipeline.save(path_results)
-            except Exception as ex:
-                self.logger.error(f'Can not save pipeline: {ex}')
+                        result_at_launch = prediction[model][launch]
+                        feature_target_dict = {'train_features.csv': result_at_launch['train_features'],
+                                               'train_target.csv': result_at_launch['train_target'],
+                                               'test_features.csv': result_at_launch['test_features'],
+                                               'test_target.csv': result_at_launch['test_target']}
 
-            for name, features in feature_target_dict.items():
-                pd.DataFrame(features).to_csv(os.path.join(result_at_launch['path_to_save'], name))
+                        try:
+                            result_at_launch['fitted_predictor'].current_pipeline.save(path_pipeline,
+                                                                                       datetime_in_path=False)
+                        except Exception as ex:
+                            self.logger.error(f'Can not save pipeline: {ex}')
 
-            if not isinstance(result_at_launch['class_probability'], pd.DataFrame):
-                df_preds = pd.DataFrame(result_at_launch['class_probability'].round(3), dtype=float)
-                df_preds.columns = [f'Class_{x+1}' for x in df_preds.columns]
-                df_preds['Target'] = result_at_launch['test_target']
-                df_preds['Predicted_labels'] = result_at_launch['label']
-            else:
-                df_preds = result_at_launch['class_probability']
-                df_preds['Target'] = result_at_launch['test_target'].values
+                        for name, features in feature_target_dict.items():
+                            pd.DataFrame(features).to_csv(os.path.join(path_features, name))
 
-            if isinstance(result_at_launch['metrics'], str):
-                df_metrics = pd.DataFrame()
-            else:
-                df_metrics = pd.DataFrame.from_records(data=[x for x in result_at_launch['metrics'].items()],
-                                                       columns=['Metric_name', 'Metric_value']).reset_index()
-                del df_metrics['index']
-                df_metrics = df_metrics.T
-                df_metrics = pd.DataFrame(df_metrics.values[1:], columns=df_metrics.iloc[0])
+                        if not isinstance(result_at_launch['class_probability'], pd.DataFrame):
+                            df_preds = pd.DataFrame(result_at_launch['class_probability'].round(3), dtype=float)
+                            df_preds.columns = [f'Class_{x + 1}' for x in df_preds.columns]
+                            df_preds['Target'] = result_at_launch['test_target']
+                            df_preds['Predicted_labels'] = result_at_launch['label']
+                        else:
+                            df_preds = result_at_launch['class_probability']
+                            df_preds['Target'] = result_at_launch['test_target'].values
 
-            for p, d in zip(['probs_preds_target.csv', 'metrics.csv'],
-                            [df_preds, df_metrics]):
-                full_path = os.path.join(path_results, p)
-                d.to_csv(full_path)
+                        if isinstance(result_at_launch['metrics'], str):
+                            df_metrics = pd.DataFrame()
+                        else:
+                            df_metrics = pd.DataFrame.from_records(data=[x for x in result_at_launch['metrics'].items()],
+                                                                   columns=['Metric_name', 'Metric_value']).reset_index()
+                            del df_metrics['index']
+                            df_metrics = df_metrics.T
+                            df_metrics = pd.DataFrame(df_metrics.values[1:], columns=df_metrics.iloc[0])
+
+                        for p, d in zip(['probs_preds_target.csv', 'metrics.csv'],
+                                        [df_preds, df_metrics]):
+                            full_path = os.path.join(path_test_results, p)
+                            d.to_csv(full_path)
 
     def save_boosting_results(self, prediction):
         for launch in prediction:
@@ -85,12 +92,11 @@ class ResultSaver:
             else:
                 self.logger.info('Ensemble model cannot be saved due to applied SUM method ')
 
-    def save_ensemble_results(self, prediction: dict):
-        for launch in prediction:
-            metric_at_launch = prediction[launch]
-            metric_df = pd.concat([pd.Series(metric_at_launch[key]).to_frame() for key in metric_at_launch], axis=1)
-            metric_df.columns = metric_at_launch.keys()
-            path_to_save_launch = os.path.join(prediction['path_to_save'], str(launch))
-            if not os.path.exists(path_to_save_launch):
-                os.makedirs(path_to_save_launch)
-            metric_df.to_csv(os.path.join(path_to_save_launch, 'ensemble_metrics.csv'))
+    def save_ensemble_results(self, prediction: dict, path: str, dataset_name: str):
+        prediction.update({'dataset': dataset_name})
+        path_to_save = os.path.join(path, 'ensemble')
+        os.makedirs(path_to_save, exist_ok=True)
+        ensemble_results = pd.DataFrame.from_records(data=[x for x in prediction.items()]).T
+        ensemble_results.to_csv(os.path.join(path_to_save, f'{dataset_name}_ensemble_results.csv'),
+                                header=False)
+        self.logger.info(f'Ensemble results saved')
