9d7f851187635ed52f41ee0ad1df7d8d2950e218
vadim_potemkin <aspirantvadim@gmail.com>
2023-05-04 16:18:34 +0200

    introduced TSSplitter class for splitting single TS into train/test data, based on anomaly labels

---
+++ fedot_ind/core/operation/transformation/splitter.py
@@ -0,0 +1,250 @@
+import math
+import random
+import re
+from typing import List, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+class TSSplitter:
+    """
+    Class for splitting single time series based on anomaly dictionary into train and test parts
+    for time series classification task.
+
+    Args:
+        time_series: time series to split
+        anomaly_dict: dictionary where keys are anomaly labels and values are anomaly index ranges.
+        strategy: strategy for splitting time series. Available strategies: 'frequent'.
+
+
+    Attributes:
+        selected_non_anomaly_intervals: list of non-anomaly intervals which were selected for splitting.
+
+    Example:
+        Create time series and anomaly dictionary::
+            ts = np.random.rand(800)
+            anomaly_d = {
+                'anomaly1': '40:50, 60:80, 200:220, 410:420, 513:524, 641:645',
+                'anomaly2': '130:170, 300:320, 400:410, 589:620, 715:720',
+                'anomaly3': '500:530, 710:740',
+                'anomaly4': '77:90, 98:112, 145:158, 290:322'}
+
+        Split time series into train and test parts::
+            from fedot_ind.core.operation.transformation.splitter import TSSplitter
+            splitter = TSSplitter(ts, anomaly_d)
+            train, test = splitter.split(plot=True, binarize=False)
+
+    """
+
+    def __init__(self, time_series: np.ndarray,
+                 anomaly_dict: dict,
+                 strategy: str = 'frequent',
+                 delimiter: str = ':'):
+
+        self.delimiter = delimiter
+        self.time_series = time_series
+        self.anomaly_dict = anomaly_dict
+        self.strategy = strategy
+        self.selected_non_anomaly_intervals = []
+
+    def split(self, plot: bool = False, binarize: bool = False) -> tuple:
+        """
+        Method for splitting time series into train and test parts.
+
+        Returns:
+            tuple with train and test parts of time series.
+
+        """
+        classes, intervals = self._get_anomaly_intervals()
+
+        if self.strategy == 'frequent':
+            freq_length = self._get_frequent_anomaly_length(intervals)
+            transformed_intervals = self._transform_intervals(intervals, freq_length)
+        else:
+            raise ValueError('Unknown strategy')
+
+        target, features = self._split_by_intervals(classes, transformed_intervals)
+
+        non_anomaly_inters = self._get_non_anomaly_intervals(transformed_intervals)
+
+        target, features = self.balance_with_non_anomaly(target, features, non_anomaly_inters)
+
+        if binarize:
+            target = self._binarize_target(target)
+
+        X_train, X_test, y_train, y_test = train_test_split(pd.DataFrame(features),
+                                                            target,
+                                                            test_size=0.2,
+                                                            random_state=42,
+                                                            stratify=target)
+
+        if plot:
+            self.plot_classes_and_intervals(classes=classes,
+                                            intervals=intervals,
+                                            transformed_intervals=transformed_intervals,
+                                            non_anomaly_intervals=non_anomaly_inters)
+
+        return (X_train, y_train), (X_test, y_test)
+
+    def _get_anomaly_intervals(self) -> Tuple[List[str], List[list]]:
+
+        labels = list(self.anomaly_dict.keys())
+        label_intervals = []
+        for anomaly_label in labels:
+            # Extract the intervals using regular expressions
+            s = self.anomaly_dict[anomaly_label]
+            intervals = re.findall(r'\d+{0}\d+'.format(re.escape(self.delimiter)), s)
+            result = [list(map(int, interval.split(self.delimiter))) for interval in intervals]
+
+            label_intervals.append(result)
+        return labels, label_intervals
+
+    def _get_frequent_anomaly_length(self, intervals: List[list]):
+        flat_intervals = []
+        for sublist in intervals:
+            for element in sublist:
+                flat_intervals.append(element)
+
+        lengths = list(map(lambda x: x[1] - x[0], flat_intervals))
+        return max(set(lengths), key=lengths.count)
+
+    def _transform_intervals(self, intervals, freq_len):
+        new_intervals = []
+        for class_inter in intervals:
+            new_class_intervals = []
+            for i in class_inter:
+                current_len = i[1] - i[0]
+                abs_diff = abs(current_len - freq_len)
+                left_add = math.ceil(abs_diff / 2)
+                right_add = math.floor(abs_diff / 2)
+                # Calculate new borders
+
+                # If current anomaly interval is less than frequent length,
+                # we expand current interval to the size of frequent
+                if current_len < freq_len:
+                    left = i[0] - left_add
+                    right = i[1] + right_add
+                    # If left border is negative, shift right border to the right
+                    if left < 0:
+                        right += abs(left)
+                        left = 0
+                    # If right border is greater than time series length, shift left border to the left
+                    if right > len(self.time_series):
+                        left -= abs(right - len(self.time_series))
+                        right = len(self.time_series)
+
+                # If current anomaly interval is greater than frequent length,
+                # we shrink current interval to the size of frequent
+                elif current_len > freq_len:
+                    left = i[0] + left_add
+                    right = i[1] - right_add
+                else:
+                    left = i[0]
+                    right = i[1]
+
+                new_class_intervals.append([left, right])
+            new_intervals.append(new_class_intervals)
+        return new_intervals
+
+    def plot_classes_and_intervals(self, classes, intervals, transformed_intervals, non_anomaly_intervals):
+        fig, axes = plt.subplots(3, 1, figsize=(17, 7))
+        fig.tight_layout()
+        for ax in axes:
+            ax.plot(self.time_series, color='black', linewidth=1, alpha=1)
+
+        axes[0].set_title('Initial intervals')
+        axes[1].set_title('Transformed intervals')
+        axes[2].set_title('Non-anomaly samples')
+
+        for i, label in enumerate(classes):
+            for interval_ in transformed_intervals[i]:
+                axes[1].axvspan(interval_[0], interval_[1], alpha=0.3, color='blue', edgecolor="black")
+                axes[1].text(interval_[0], 0.5, label, fontsize=12, rotation=90)
+            for interval in intervals[i]:
+                axes[0].axvspan(interval[0], interval[1], alpha=0.3, color='red', edgecolor="black")
+                axes[0].text(interval[0], 0.5, label, fontsize=12, rotation=90)
+
+        if self.selected_non_anomaly_intervals is not None:
+            for interval in self.selected_non_anomaly_intervals:
+                axes[2].axvspan(interval[0], interval[1], alpha=0.3, color='green', edgecolor="black")
+                axes[2].text(interval[0], 0.5, 'no_anomaly', fontsize=12, rotation=90)
+        plt.show()
+
+    def _split_by_intervals(self, classes, transformed_intervals):
+        all_labels, all_ts = [], []
+
+        for i, label in enumerate(classes):
+            for inter in transformed_intervals[i]:
+                all_labels.append(label)
+                all_ts.append(self.time_series[inter[0]:inter[1]])
+        return all_labels, all_ts
+
+    def _binarize_target(self, target):
+        new_target = []
+        for label in target:
+            if label == 'no_anomaly':
+                new_target.append(0)
+            else:
+                new_target.append(1)
+        return new_target
+
+    def balance_with_non_anomaly(self, target, features, non_anomaly_intervals):
+        number_of_anomalies = len(target)
+        anomaly_len = len(features[0])
+        non_anomaly_ts_list = []
+        ts = self.time_series.copy()
+        counter = 0
+        while len(non_anomaly_ts_list) != number_of_anomalies and counter != number_of_anomalies*100:
+            seed = np.random.randint(1000)
+            random.seed(seed)
+            random_inter = random.choice(non_anomaly_intervals)
+            cropped_ts_len = random_inter[1] - random_inter[0]
+            counter += 1
+            if cropped_ts_len < anomaly_len:
+                continue
+            random_start_index = random.randint(random_inter[0], random_inter[0] + cropped_ts_len - anomaly_len)
+            stop_index = random_start_index + anomaly_len
+            non_anomaly_ts = ts[random_start_index:random_start_index + anomaly_len]
+            non_anomaly_ts_list.append(non_anomaly_ts)
+            self.selected_non_anomaly_intervals.append([random_start_index, random_start_index + anomaly_len])
+
+        if len(non_anomaly_ts_list) == 0:
+            raise Exception('No non-anomaly intervals found')
+
+        target.extend(['no_anomaly'] * len(non_anomaly_ts_list))
+        features.extend(non_anomaly_ts_list)
+
+        return target, features
+
+    def _get_non_anomaly_intervals(self, anom_intervals: List[list]):
+        flat_intervals_list = []
+        for sublist in anom_intervals:
+            for element in sublist:
+                flat_intervals_list.append(element)
+
+        series = pd.Series(self.time_series).copy()
+        for single_interval in flat_intervals_list:
+            series[single_interval[0]:single_interval[1]] = np.nan
+
+        non_nan_intervals = []
+        for k, g in series.groupby((series.notnull() != series.shift().notnull()).cumsum()):
+            if g.notnull().any():
+                non_nan_intervals.append((g.index[0], g.index[-1]))
+        return non_nan_intervals
+
+
+if __name__ == '__main__':
+    ts = np.random.rand(800)
+    anomaly_d = {
+        'anomaly1': '40:50, 60:80, 200:220, 410:420, 513:524, 641:645',
+        'anomaly2': '130:170, 300:320, 400:410, 589:620, 715:720',
+        'anomaly3': '500:530, 710:740',
+        'anomaly4': '77:90, 98:112, 145:158, 290:322',
+    }
+
+    splitter = TSSplitter(ts, anomaly_d)
+    train, test = splitter.split(plot=True, binarize=False)
+    _ = 1
