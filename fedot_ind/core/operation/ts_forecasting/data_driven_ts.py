63fd7fadc24dd8a7f472fa01648f534131a7429f
valer1435 <valp998@gmail.com>
2023-06-22 11:05:05 +0300

    repaired ar method

--- fedot_ind/core/operation/ts_forecasting/data_driven_ts.py
+++ fedot_ind/core/operation/ts_forecasting/data_driven_ts.py
@@ -3,8 +3,15 @@ from typing import TypeVar, Optional
 from fedot.core.data.data import InputData, OutputData
 from fedot.core.operations.evaluation.operation_implementations.implementation_interfaces import ModelImplementation
 from fedot.core.operations.operation_parameters import OperationParameters
+from fedot.core.pipelines.pipeline_builder import PipelineBuilder
+from fedot.core.pipelines.tuning.tuner_builder import TunerBuilder
+from fedot.core.repository.quality_metrics_repository import RegressionMetricsEnum
+from golem.core.tuning.simultaneous import SimultaneousTuner
 from matplotlib import pyplot as plt
 from pymonad.either import Either
+
+from fedot_ind.core.operation.decomposition.SpectrumDecomposition import SpectrumDecomposer
+from fedot_ind.core.operation.transformation.data.hankel import HankelMatrix
 
 class_type = TypeVar("T", bound="DataDrivenBasis")
 
@@ -253,20 +260,56 @@ class DataDrivenForForecastingBasisImplementation(ModelImplementation):
         self.n_components = params.get('n_components', 3)
         self.window_size = params.get('window_size')
         self.seasonality = params.get('seasonality')
-        self.estimator = params.get('estimator')
+        self.estimator = params.get('estimator', PipelineBuilder().add_node('ar').build())
         self.decomposer = None
         self.basis = None
 
+    # def predict(self, input_data: InputData) -> OutputData:
+    #     forecast_length = input_data.task.task_params.forecast_length
+    #     ssa = mySSA(input_data.features)
+    #     ssa.embed(embedding_dimension=self.window_size,
+    #               suspected_frequency=int(self.seasonality * self.window_size / 100))
+    #     ssa.decompose()
+    #     plt.show()
+    #     predict = ssa.forecast_recurrent(steps_ahead=forecast_length,
+    #                                      singular_values=[i for i in range(min(self.n_components, ssa.r))])
+    #     return predict.values
+
+
     def predict(self, input_data: InputData) -> OutputData:
-        forecast_length = input_data.task.task_params.forecast_length
-        ssa = mySSA(input_data.features)
-        ssa.embed(embedding_dimension=self.window_size,
-                  suspected_frequency=int(self.seasonality * self.window_size / 100))
-        ssa.decompose()
-        plt.show()
-        predict = ssa.forecast_recurrent(steps_ahead=forecast_length,
-                                         singular_values=[i for i in range(min(self.n_components, ssa.r))])
-        return predict.values
+        trajectory_transformer = HankelMatrix(time_series=input_data.features, window_size=self.window_size)
+        data = trajectory_transformer.trajectory_matrix
+        self.window_size = trajectory_transformer.window_length
+        self.decomposer = SpectrumDecomposer(data, self.n_components, trajectory_transformer.ts_length)
+        U, s, VT = self.get_svd(data)
+        s[1] = np.sqrt(s[1])
+        components = self.get_combined_components(U, s, VT)
+        reconstructed_target = []
+        components = components[:self.n_components]
+        print(len(components))
+        for i, row in enumerate(components):
+            # auto_model = Fedot(problem='ts_forecasting', task_params=input_data.task.task_params,
+            #                    timeout=0.5,
+            #                    n_jobs=1)
+            train_data = InputData(idx=np.arange(row.shape[0]), features=row, target=row,
+                                   data_type=input_data.data_type,
+                                   task=input_data.task)
+            test_data = InputData(idx=input_data.idx, features=row, target=None,
+                                  data_type=input_data.data_type,
+                                  task=input_data.task)
+            pipeline_tuner = TunerBuilder(train_data.task) \
+                .with_tuner(SimultaneousTuner) \
+                .with_metric(RegressionMetricsEnum.MAE) \
+                .with_iterations(5) \
+                .with_cv_folds(22)\
+                .build(train_data)
+            self.estimator = pipeline_tuner.tune(self.estimator)
+            self.estimator.fit(train_data)
+            predict = np.ravel(self.estimator.predict(test_data).predict)
+            reconstructed_target.append(predict)
+            self.estimator.unfit()
+        return np.array(reconstructed_target).sum(axis=0)
+
 
     def fit(self, input_data: InputData):
         pass
