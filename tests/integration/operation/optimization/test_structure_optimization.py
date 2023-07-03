79c9ca9410980bc08e577a6150f7fb40ec257c6f
N-31.V <litvintseva.ann@gmail.com>
2023-07-03 18:57:14 +0300

    update test_structure_optimization.py for different forward_mode

--- tests/integration/operation/optimization/test_structure_optimization.py
+++ tests/integration/operation/optimization/test_structure_optimization.py
@@ -12,6 +12,29 @@ SFP_PERCENTAGE_PARAMS = {'zeroing_mode': 'percentage', 'zeroing_mode_params': {'
 SFP_ENERGY_PARAMS = {'zeroing_mode': 'energy', 'zeroing_mode_params': {'energy_threshold': 0.9}}
 
 
+def check_sfp_paths(models, summary, classification=True):
+    assert os.path.exists(models.joinpath('train.sd.pt'))
+    assert os.path.exists(summary.joinpath('train/train.csv'))
+    assert os.path.exists(summary.joinpath('train/val.csv'))
+
+    if classification:
+        assert os.path.exists(models.joinpath('pruned.sd.pt'))
+        assert os.path.exists(summary.joinpath('size.csv'))
+        assert os.path.exists(summary.joinpath('pruned/train.csv'))
+        assert os.path.exists(summary.joinpath('pruned/val.csv'))
+
+
+def check_svd_paths(models, summary, classification=True):
+    assert os.path.exists(models.joinpath('train.sd.pt'))
+    assert os.path.exists(models.joinpath('trained.model.pt'))
+    assert os.path.exists(models.joinpath('e_0.9.sd.pt'))
+    assert os.path.exists(summary.joinpath('train/train.csv'))
+    assert os.path.exists(summary.joinpath('train/val.csv'))
+    assert os.path.exists(summary.joinpath('size.csv'))
+    assert os.path.exists(summary.joinpath('pruning.csv'))
+    assert os.path.exists(summary.joinpath('e_0.9/train.csv'))
+    assert os.path.exists(summary.joinpath('e_0.9/val.csv'))
+
 def test_sfp_percentage_classification_experimenter(prepare_classification):
     exp_params, fit_params, tmp_path = prepare_classification
     experimenter = ClassificationExperimenter(**exp_params)
@@ -19,14 +42,7 @@ def test_sfp_percentage_classification_experimenter(prepare_classification):
     optimization.fit(exp=experimenter, params=fit_params, ft_params=fit_params)
     models = tmp_path.joinpath('models/Agricultural/ResNet_SFP_pruning_ratio-0.5/')
     summary = tmp_path.joinpath('summary/Agricultural/ResNet_SFP_pruning_ratio-0.5/')
-
-    assert os.path.exists(models.joinpath('train.sd.pt'))
-    assert os.path.exists(models.joinpath('pruned.sd.pt'))
-    assert os.path.exists(summary.joinpath('train/train.csv'))
-    assert os.path.exists(summary.joinpath('train/val.csv'))
-    assert os.path.exists(summary.joinpath('size.csv'))
-    assert os.path.exists(summary.joinpath('pruned/train.csv'))
-    assert os.path.exists(summary.joinpath('pruned/val.csv'))
+    check_sfp_paths(models, summary)
     classification_predict(experimenter)
 
 
@@ -37,14 +53,7 @@ def test_sfp_energy_classification_experimenter(prepare_classification):
     optimization.fit(exp=experimenter, params=fit_params, ft_params=fit_params)
     models = tmp_path.joinpath('models/Agricultural/ResNet_SFP_energy_threshold-0.9/')
     summary = tmp_path.joinpath('summary/Agricultural/ResNet_SFP_energy_threshold-0.9/')
-
-    assert os.path.exists(models.joinpath('train.sd.pt'))
-    assert os.path.exists(models.joinpath('pruned.sd.pt'))
-    assert os.path.exists(summary.joinpath('train/train.csv'))
-    assert os.path.exists(summary.joinpath('train/val.csv'))
-    assert os.path.exists(summary.joinpath('size.csv'))
-    assert os.path.exists(summary.joinpath('pruned/train.csv'))
-    assert os.path.exists(summary.joinpath('pruned/val.csv'))
+    check_sfp_paths(models, summary)
     classification_predict(experimenter)
 
 
@@ -55,16 +64,29 @@ def test_svd_channel_classification_experimenter(prepare_classification):
     optimization.fit(exp=experimenter, params=fit_params, ft_params=fit_params)
     models = tmp_path.joinpath('models/Agricultural/ResNet_SVD_channel_O-10_H-0.1/')
     summary = tmp_path.joinpath('summary/Agricultural/ResNet_SVD_channel_O-10_H-0.1/')
+    check_svd_paths(models, summary)
+    classification_predict(experimenter)
 
-    assert os.path.exists(models.joinpath('train.sd.pt'))
-    assert os.path.exists(models.joinpath('trained.model.pt'))
-    assert os.path.exists(models.joinpath('e_0.9.sd.pt'))
-    assert os.path.exists(summary.joinpath('train/train.csv'))
-    assert os.path.exists(summary.joinpath('train/val.csv'))
-    assert os.path.exists(summary.joinpath('size.csv'))
-    assert os.path.exists(summary.joinpath('pruning.csv'))
-    assert os.path.exists(summary.joinpath('e_0.9/train.csv'))
-    assert os.path.exists(summary.joinpath('e_0.9/val.csv'))
+
+def test_svd_channel_classification_experimenter_two_layers(prepare_classification):
+    exp_params, fit_params, tmp_path = prepare_classification
+    experimenter = ClassificationExperimenter(**exp_params)
+    optimization = SVDOptimization(decomposing_mode='channel', forward_mode='two_layers', **SVD_PARAMS)
+    optimization.fit(exp=experimenter, params=fit_params, ft_params=fit_params)
+    models = tmp_path.joinpath('models/Agricultural/ResNet_SVD_channel_O-10_H-0.1/')
+    summary = tmp_path.joinpath('summary/Agricultural/ResNet_SVD_channel_O-10_H-0.1/')
+    check_svd_paths(models, summary)
+    classification_predict(experimenter)
+
+
+def test_svd_channel_classification_experimenter_three_layers(prepare_classification):
+    exp_params, fit_params, tmp_path = prepare_classification
+    experimenter = ClassificationExperimenter(**exp_params)
+    optimization = SVDOptimization(decomposing_mode='channel', forward_mode='three_layers', **SVD_PARAMS)
+    optimization.fit(exp=experimenter, params=fit_params, ft_params=fit_params)
+    models = tmp_path.joinpath('models/Agricultural/ResNet_SVD_channel_O-10_H-0.1/')
+    summary = tmp_path.joinpath('summary/Agricultural/ResNet_SVD_channel_O-10_H-0.1/')
+    check_svd_paths(models, summary)
     classification_predict(experimenter)
 
 
@@ -75,16 +97,7 @@ def test_svd_spatial_classification_experimenter(prepare_classification):
     optimization.fit(exp=experimenter, params=fit_params, ft_params=fit_params)
     models = tmp_path.joinpath('models/Agricultural/ResNet_SVD_spatial_O-10_H-0.1/')
     summary = tmp_path.joinpath('summary/Agricultural/ResNet_SVD_spatial_O-10_H-0.1/')
-
-    assert os.path.exists(models.joinpath('train.sd.pt'))
-    assert os.path.exists(models.joinpath('trained.model.pt'))
-    assert os.path.exists(models.joinpath('e_0.9.sd.pt'))
-    assert os.path.exists(summary.joinpath('train/train.csv'))
-    assert os.path.exists(summary.joinpath('train/val.csv'))
-    assert os.path.exists(summary.joinpath('size.csv'))
-    assert os.path.exists(summary.joinpath('pruning.csv'))
-    assert os.path.exists(summary.joinpath('e_0.9/train.csv'))
-    assert os.path.exists(summary.joinpath('e_0.9/val.csv'))
+    check_svd_paths(models, summary)
     classification_predict(experimenter)
 
 
@@ -95,10 +108,7 @@ def test_sfp_percentage_objectdetection_experimenter(prepare_detection):
     optimization.fit(exp=experimenter, params=fit_params)
     models = tmp_path.joinpath('models/ALET10/SSD_SFP_pruning_ratio-0.5/')
     summary = tmp_path.joinpath('summary/ALET10/SSD_SFP_pruning_ratio-0.5/')
-
-    assert os.path.exists(models.joinpath('train.sd.pt'))
-    assert os.path.exists(summary.joinpath('train/train.csv'))
-    assert os.path.exists(summary.joinpath('train/val.csv'))
+    check_sfp_paths(models, summary, classification=False)
     detection_predict(experimenter)
 
 
@@ -109,10 +119,7 @@ def test_sfp_enegry_objectdetection_experimenter(prepare_detection):
     optimization.fit(exp=experimenter, params=fit_params)
     models = tmp_path.joinpath('models/ALET10/SSD_SFP_energy_threshold-0.9/')
     summary = tmp_path.joinpath('summary/ALET10/SSD_SFP_energy_threshold-0.9/')
-
-    assert os.path.exists(models.joinpath('train.sd.pt'))
-    assert os.path.exists(summary.joinpath('train/train.csv'))
-    assert os.path.exists(summary.joinpath('train/val.csv'))
+    check_sfp_paths(models, summary, classification=False)
     detection_predict(experimenter)
 
 
@@ -123,16 +130,7 @@ def test_svd_channel_objectdetection_experimenter(prepare_detection):
     optimization.fit(exp=experimenter, params=fit_params, ft_params=fit_params)
     models = tmp_path.joinpath('models/ALET10/SSD_SVD_channel_O-10_H-0.1/')
     summary = tmp_path.joinpath('summary/ALET10/SSD_SVD_channel_O-10_H-0.1/')
-
-    assert os.path.exists(models.joinpath('train.sd.pt'))
-    assert os.path.exists(models.joinpath('trained.model.pt'))
-    assert os.path.exists(models.joinpath('e_0.9.sd.pt'))
-    assert os.path.exists(summary.joinpath('train/train.csv'))
-    assert os.path.exists(summary.joinpath('train/val.csv'))
-    assert os.path.exists(summary.joinpath('size.csv'))
-    assert os.path.exists(summary.joinpath('pruning.csv'))
-    assert os.path.exists(summary.joinpath('e_0.9/train.csv'))
-    assert os.path.exists(summary.joinpath('e_0.9/val.csv'))
+    check_svd_paths(models, summary)
     detection_predict(experimenter)
 
 
@@ -143,14 +141,5 @@ def test_svd_spatial_objectdetection_experimenter(prepare_detection):
     optimization.fit(exp=experimenter, params=fit_params, ft_params=fit_params)
     models = tmp_path.joinpath('models/ALET10/SSD_SVD_spatial_O-10_H-0.1/')
     summary = tmp_path.joinpath('summary/ALET10/SSD_SVD_spatial_O-10_H-0.1/')
-
-    assert os.path.exists(models.joinpath('train.sd.pt'))
-    assert os.path.exists(models.joinpath('trained.model.pt'))
-    assert os.path.exists(models.joinpath('e_0.9.sd.pt'))
-    assert os.path.exists(summary.joinpath('train/train.csv'))
-    assert os.path.exists(summary.joinpath('train/val.csv'))
-    assert os.path.exists(summary.joinpath('size.csv'))
-    assert os.path.exists(summary.joinpath('pruning.csv'))
-    assert os.path.exists(summary.joinpath('e_0.9/train.csv'))
-    assert os.path.exists(summary.joinpath('e_0.9/val.csv'))
+    check_svd_paths(models, summary)
     detection_predict(experimenter)
