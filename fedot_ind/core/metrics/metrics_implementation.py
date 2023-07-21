fixed metrics calculation for anomalies detection, added feature space reduction by variability

fedot_ind/core/metrics/metrics_implementation.py
@@ -41,8 +41,8 @@ class QualityMetric:
                  metric_list: list = ('f1', 'roc_auc', 'accuracy', 'logloss', 'precision'),
                  default_value: float = 0.0):
         self.predicted_probs = predicted_probs
-        self.predicted_labels = predicted_labels.reshape(-1)
-        self.target = target.reshape(-1)
+        self.predicted_labels = np.array(predicted_labels).flatten()
+        self.target = np.array(target).flatten()
         self.metric_list = metric_list
         self.default_value = default_value
 

fedot_ind/core/models/statistical/StatsExtractor.py
@@ -51,14 +51,13 @@ class StatsExtractor(BaseExtractor):
         stat_features = v[0].columns
         n_components = v[0].shape[0]
         predict = self._clean_predict(np.array(v))
-        # predict = self.drop_features(predict=predict,
-        #                              target_values=input_data.target,
-        #                              columns=stat_features,
-        #                              n_components=n_components)
-        return predict
-        # return predict.values
+        predict = self.drop_features(predict=predict,
+                                     columns=stat_features,
+                                     n_components=n_components)
+        # return predict
+        return predict.values
 
-    def drop_features(self, predict: pd.DataFrame, target_values, columns: Index, n_components: int):
+    def drop_features(self, predict: pd.DataFrame, columns: Index, n_components: int):
         """
         Method for dropping features with low variance
         """