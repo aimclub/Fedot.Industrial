11c42d981eb7f40c49b84a99521f6bb5be9e29fd
v1docq <ir@raxeltelematics.com>
2023-08-08 12:58:17 +0300

    refactor experiments

--- fedot_ind/api/multi_ts_example.py
+++ fedot_ind/api/multi_ts_example.py
@@ -1,67 +1,197 @@
+import os
+import sys
+
 import numpy as np
+import pandas as pd
 from fedot.api.main import Fedot
 from fedot.core.data.data import InputData
 from fedot.core.repository.dataset_types import DataTypesEnum
 from fedot.core.repository.tasks import Task, TaskTypesEnum
-from sklearn.decomposition import PCA
-from sklearn.metrics import mean_squared_error
+from fedot.core.pipelines.node import PipelineNode
+from fedot.core.pipelines.pipeline import Pipeline
+from matplotlib import pyplot as plt
 
 from fedot_ind.core.architecture.preprocessing.DatasetLoader import DataLoader
+from fedot_ind.core.models.recurrence.RecurrenceExtractor import RecurrenceExtractor
 from fedot_ind.core.models.quantile.quantile_extractor import QuantileExtractor
+from fedot_ind.core.operation.transformation.basis.data_driven import DataDrivenBasisImplementation
+from fedot_ind.core.operation.transformation.basis.fourier import FourierBasisImplementation
+from sklearn.decomposition import PCA
+from sklearn.metrics import explained_variance_score, max_error, mean_absolute_error, \
+    mean_squared_error, d2_absolute_error_score, \
+    median_absolute_error, r2_score
 
 
 def init_input(X, y):
     input_data = InputData(idx=np.arange(len(X)),
                            features=np.array(X.values.tolist()),
                            target=y.reshape(-1, 1),
-                           task=Task(TaskTypesEnum.regression),
+                           task=Task(TaskTypesEnum.classification),
                            data_type=DataTypesEnum.image)
+    input_data.features = np.where(np.isnan(input_data.features), 0, input_data.features)
+    input_data.features = np.where(np.isinf(input_data.features), 0, input_data.features)
     return input_data
 
 
-if __name__ == "__main__":
+def prepare_features(dataset_name,
+                     pca_n_components: float = 0.95,
+                     feature_generator: list = ['statistical'],
+                     reduce_dimension: bool = False):
+    train_data, test_data = DataLoader(dataset_name=dataset_name).load_data()
 
-    components = [0.90, 0.92, 0.94, 0.96, 0.98]
-    metrics = []
-    for comps in components:
-        dataset_name = 'AppliancesEnergy'
-        pca_n_components = comps
+    train_target = np.array([float(i) for i in train_data[1]])
+    test_target = np.array([float(i) for i in test_data[1]])
+    train_input_data = init_input(train_data[0], train_target)
+    test_input_data = init_input(test_data[0], test_target)
 
-        train_data, test_data = DataLoader(dataset_name=dataset_name).load_data()
-        train_target = np.array([float(i) for i in train_data[1]])
-        test_target = np.array([float(i) for i in test_data[1]])
-        train_input_data = init_input(train_data[0], train_target)
-        test_input_data = init_input(test_data[0], test_target)
-
-        extractor = QuantileExtractor({'window_mode': False,
-                                       'window_size': None,
-                                       'var_threshold': 0})
-        pca = PCA(n_components=pca_n_components,
-                  svd_solver='full')
-
+    generator_dict = {'statistical': QuantileExtractor({'window_mode': False,
+                                                        'window_size': 10,
+                                                        'var_threshold': 0}),
+                      'reccurence': RecurrenceExtractor({'window_mode': True,
+                                                         'min_signal_ratio': 0.7,
+                                                         'max_signal_ratio': 0.9,
+                                                         'rec_metric': 'cosine'
+                                                         })
+                      }
+    train_features_list, test_features_list = [], []
+    for extractor in feature_generator:
+        extractor = generator_dict[extractor]
         extracted_features_train = extractor.transform(train_input_data)
-
-        train_size = extracted_features_train.features.shape
-        train_features = extracted_features_train.features.reshape(train_size[0], train_size[1] * train_size[2])
-        train_features = pca.fit_transform(train_features)
+        train_size = extracted_features_train.predict.shape
+        train_features = extracted_features_train.predict
 
         extracted_features_test = extractor.transform(test_input_data)
-        test_size = extracted_features_test.features.shape
-        test_features = extracted_features_test.features.reshape(test_size[0], test_size[1] * test_size[2])
-        test_features = pca.transform(test_features)
+        test_size = extracted_features_test.predict.shape
+        test_features = extracted_features_test.predict
 
-        predictor = Fedot(problem='regression', timeout=10, logging_level=20, metric='rmse')
-        model = predictor.fit(features=train_features, target=train_target)
-        labels = predictor.predict(features=test_features)
+        if reduce_dimension:
+            pca = PCA(n_components=pca_n_components,
+                      svd_solver='full')
+            train_features = pca.fit_transform(train_features)
+            test_features = pca.transform(test_features)
+        train_features_list.append(train_features), test_features_list.append(test_features)
 
-        root_mean_squared_error = np.sqrt(mean_squared_error(test_target, labels))
-        metrics.append(root_mean_squared_error)
+    return train_features_list, train_target, test_features_list, test_target
 
-    import matplotlib.pyplot as plt
 
-    plt.plot(components, metrics)
-    plt.xlabel('explained variance')
-    plt.ylabel('rmse')
-    plt.show()
+def calculate_metric(test_target, labels):
+    metric_dict = {'r2_score:': r2_score(test_target, labels),
+                   'mean_squared_error:': mean_squared_error(test_target, labels),
+                   'root_mean_squared_error:': np.sqrt(mean_squared_error(test_target, labels)),
+                   'mean_absolute_error': mean_absolute_error(test_target, labels),
+                   'median_absolute_error': median_absolute_error(test_target, labels),
+                   'explained_variance_score': explained_variance_score(test_target, labels),
+                   'max_error': max_error(test_target, labels),
+                   # 'mean_poisson_deviance': mean_poisson_deviance(test_target, labels),
+                   # 'mean_gamma_deviance': mean_gamma_deviance(test_target, labels),
+                   'd2_absolute_error_score': d2_absolute_error_score(test_target, labels)
+                   }
+    df = pd.DataFrame.from_dict(metric_dict, orient='index')
+    return df
+
+
+def evaluate_baseline(train, train_target, test, test_target):
+    node_scaling = PipelineNode('scaling')
+    node_rfr = PipelineNode('lasso', nodes_from=[node_scaling])
+    baseline_model = Pipeline(node_rfr)
+    input_fit = InputData(idx=np.arange(len(train)),
+                          features=train,
+                          target=train_target.reshape(-1, 1),
+                          task=Task(TaskTypesEnum.regression),
+                          data_type=DataTypesEnum.image)
+    input_predict = InputData(idx=np.arange(len(test)),
+                              features=test,
+                              target=test_target.reshape(-1, 1),
+                              task=Task(TaskTypesEnum.regression),
+                              data_type=DataTypesEnum.image)
+
+    baseline_model.fit(input_fit)
+    labels_baseline = baseline_model.predict(input_predict).predict
+    metric_df_baseline = calculate_metric(test_target, labels_baseline)
+    return metric_df_baseline
+
+    # ddb_features_train = DataDrivenBasisImplementation({'window_size': 30,
+    #                                                     'sv_selector': 'median'}).transform(train_input_data)
+    # fourier_features_train = FourierBasisImplementation({"spectrum_type": "smoothed",
+    #                                                      "threshold": 20000}).transform(train_input_data)
+    # fourier_features_test = FourierBasisImplementation({"spectrum_type": "smoothed",
+    #                                                     "threshold": 20000}).transform(test_input_data)
+
+
+if __name__ == "__main__":
+    dataset_list = [
+        # 'Gazprom',
+        # 'AppliancesEnergy',
+        # 'AustraliaRainfall',
+       # 'BeijingPM10Quality',
+        #'BeijingPM25Quality',
+        #'BenzeneConcentration',
+        # 'HouseholdPowerConsumption1',
+        #'HouseholdPowerConsumption2',
+        #'IEEEPPG',
+        #'FloodModeling1',
+        'FloodModeling2',
+        'FloodModeling3'
+        'LiveFuelMoistureContent',
+        'BIDMC32HR',
+        'BIDMC32RR',
+        'BIDMC32SpO2',
+        'DailyOilGasPrices',
+        'ElectricityPredictor',
+        'OccupancyDetectionLight',
+        'SolarRadiationAndalusia',
+        'TetuanEnergyConsumption',
+        'WindTurbinePower',
+        'ElectricMotorTemperature',
+        'LPGasMonitoringHomeActivity',
+        'GasSensorArrayAcetone',
+        'GasSensorArrayEthanol',
+        'WaveTensionData'
+
+    ]
+    ten_minutes = range(0, 3, 1)
+    one_hour = ['1hr']
+    for dataset_name in dataset_list:
+        try:
+            os.makedirs(f'./{dataset_name}')
+        except Exception:
+            _ = 1
+
+        train_features, train_target, test_features, test_target = prepare_features(dataset_name=dataset_name,
+                                                                                    reduce_dimension=False,
+                                                                                    feature_generator=[
+                                                                                        'statistical'
+                                                                                        # ,'reccurence'
+                                                                                    ])
+        if len(train_features) > 1:
+            concatenate_train = np.concatenate(train_features, axis=1)
+            concatenate_test = np.concatenate(test_features, axis=1)
+            train_features.append(concatenate_train)
+            test_features.append(concatenate_test)
+        else:
+            concatenate_train = train_features[0]
+            concatenate_test = test_features[0]
+
+        for train, test in zip(train_features, test_features):
+            metric_df_baseline = evaluate_baseline(train, train_target, test, test_target)
+            print(metric_df_baseline)
+        metric_df_baseline.to_csv(f'./{dataset_name}/baseline_metrics.csv')
+
+        for run in one_hour:
+            predictor = Fedot(problem='regression',
+                              metric='rmse',
+                              timeout=60,
+                              early_stopping_timeout=30,
+                              logging_level=20,
+                              n_jobs=6)
+            model = predictor.fit(features=concatenate_train, target=train_target)
+            labels = predictor.predict(features=concatenate_test)
+            metric_df = calculate_metric(test_target, labels)
+            metric_df.to_csv(f'./{dataset_name}/metrics_run_{run}.csv')
+            pipeline = predictor.current_pipeline
+            pipeline.show(f'./{dataset_name}/pipeline_structure_{run}.png')
+            predictor.history.save(f'./{dataset_name}/history_run_{run}.json')
+            path_to_save = f'./{dataset_name}/saved_pipelines_run_{run}'
+            pipeline.save(path=path_to_save, create_subdir=True, is_datetime_in_path=True)
 
     _ = 1
--- fedot_ind/core/operation/transformation/DataTransformer.py
+++ fedot_ind/core/operation/transformation/DataTransformer.py
@@ -191,24 +191,23 @@ class TSTransformer:
     def __init__(self, time_series, min_signal_ratio, max_signal_ratio, rec_metric):
         self.time_series = time_series
         self.recurrence_matrix = None
-        self.threshold_baseline = [1, 5, 10, 15, 20, 25, 30]
+        self.threshold_baseline = [0.95, 0.7] #cosine
         self.min_signal_ratio = min_signal_ratio
         self.max_signal_ratio = max_signal_ratio
         self.rec_metric = rec_metric
 
     def ts_to_recurrence_matrix(self,
-                                eps=0.10,
-                                steps=None):
-        distance_matrix = pdist(metric=self.rec_metric, X=self.time_series[:, None])
-        distance_matrix = np.floor(distance_matrix / eps)
-        distance_matrix, steps = self.binarization(distance_matrix, threshold=steps)
-        distance_matrix[distance_matrix > steps] = steps
+                                threshold=None):
+        distance_matrix = pdist(metric=self.rec_metric, X=self.time_series.T)
+        distance_matrix = np.ones(shape=distance_matrix.shape[0])-distance_matrix
+        distance_matrix = self.binarization(distance_matrix, threshold=threshold)
         self.recurrence_matrix = squareform(distance_matrix)
         return self.recurrence_matrix
 
     def binarization(self, distance_matrix, threshold):
         best_threshold_flag = False
         signal_ratio_list = []
+        reccurence_matrix = None
         if threshold is None:
             for threshold_baseline in self.threshold_baseline:
                 threshold = threshold_baseline
@@ -219,20 +218,20 @@ class TSTransformer:
 
                 if self.min_signal_ratio < signal_ratio < self.max_signal_ratio:
                     best_ratio = signal_ratio
-                    distance_matrix = tmp_array
+                    reccurence_matrix = tmp_array
                     best_threshold_flag = True
                     if signal_ratio > best_ratio:
-                        distance_matrix = tmp_array
+                        reccurence_matrix = tmp_array
                 else:
                     signal_ratio_list.append(abs(self.max_signal_ratio - signal_ratio))
 
                 del tmp_array
 
         if not best_threshold_flag:
-            threshold = self.threshold_baseline[signal_ratio_list.index(min(signal_ratio_list))]
-            distance_matrix[distance_matrix < threshold] = 0.0
-            distance_matrix[distance_matrix >= threshold] = 1.0
-        return distance_matrix, threshold
+            distance_matrix[distance_matrix < self.threshold_baseline[0]] = 0.0
+            distance_matrix[distance_matrix >= self.threshold_baseline[0]] = 1.0
+            reccurence_matrix = distance_matrix
+        return reccurence_matrix
 
     def get_recurrence_metrics(self):
         if self.recurrence_matrix is None:
