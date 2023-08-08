4d2a0528fb9d516b6e2318b13daefa8674830e99
v1docq <ir@raxeltelematics.com>
2023-08-08 18:00:49 +0300

    refactor wavelet_extractor

--- fedot_ind/core/models/quantile/quantile_extractor.py
+++ fedot_ind/core/models/quantile/quantile_extractor.py
@@ -1,3 +1,4 @@
+import datetime
 from multiprocessing import Pool
 from typing import Optional
 
@@ -5,6 +6,7 @@ import numpy as np
 import pandas as pd
 from fedot.core.data.data import InputData
 from fedot.core.operations.operation_parameters import OperationParameters
+from joblib import Parallel, delayed
 from pandas import Index
 from tqdm import tqdm
 
@@ -20,10 +22,10 @@ class QuantileExtractor(BaseExtractor):
 
     def __init__(self, params: Optional[OperationParameters] = None):
         super().__init__(params)
-        self.var_threshold = params.get('var_threshold')
+        # self.var_threshold = params.get('var_threshold')
         self.window_mode = params.get('window_mode')
         self.window_size = params.get('window_size')
-
+        self.var_threshold = 0.1
         self.logging_params.update({'Wsize': self.window_size,
                                     'Wmode': self.window_mode,
                                     'VarTh': self.var_threshold})
@@ -37,30 +39,18 @@ class QuantileExtractor(BaseExtractor):
         Method for feature generation for all series
         """
         try:
-            input_data_squeezed = np.squeeze(input_data.features, 3)
-        except ValueError:
-            input_data_squeezed = input_data.features
-
-        with Pool(self.n_processes) as p:
-            v = list(tqdm(p.imap(self.generate_features_from_ts, input_data_squeezed),
-                          total=input_data.features.shape[0],
-                          desc=f'{self.__class__.__name__} transform',
-                          postfix=f'{self.logging_params}',
-                          colour='green',
-                          unit='ts',
-                          ascii=False,
-                          position=0,
-                          leave=True)
-                     )
+            input_data_squeezed = np.squeeze(input_data.features)
+        except Exception:
+            input_data_squeezed = np.squeeze(input_data)
+        parallel = Parallel(n_jobs=self.n_processes, verbose=0, pre_dispatch="2*n_jobs")
+        v = parallel(delayed(self.generate_features_from_ts)(sample) for sample in input_data_squeezed)
         stat_features = v[0].columns
         n_components = v[0].shape[0]
         predict = self._clean_predict(np.array(v))
-        predict = self.drop_features(predict=predict,
-                                     columns=stat_features,
-                                     n_components=n_components)
-        # percent of feature space reduction
-        self.logger.info(f'Feature space reduced by {len(stat_features)*n_components / predict.shape[1]}%')
-        return predict.values
+        # predict = self.drop_features(predict=predict,
+        #                              columns=stat_features,
+        #                              n_components=n_components)
+        return predict
 
     def drop_features(self, predict: pd.DataFrame, columns: Index, n_components: int):
         """
@@ -68,7 +58,7 @@ class QuantileExtractor(BaseExtractor):
         """
         # Fill columns names for every extracted ts component
         predict = pd.DataFrame(predict,
-                               columns=[f'{col}{str(i)}' for i in range(1, n_components+1) for col in columns])
+                               columns=[f'{col}{str(i)}' for i in range(1, n_components + 1) for col in columns])
 
         if self.relevant_features is None:
             reduced_df, self.relevant_features = self.filter_by_var(predict, threshold=self.var_threshold)
@@ -88,13 +78,18 @@ class QuantileExtractor(BaseExtractor):
 
     def extract_stats_features(self, ts):
         if self.window_mode:
-            # aggregator = self.aggregator.create_baseline_features
+            global_features = self.get_statistical_features(ts, add_global_features=True)
             list_of_stat_features = self.apply_window_for_stat_feature(ts_data=ts.T if ts.shape[1] == 1 else ts,
                                                                        feature_generator=self.get_statistical_features,
                                                                        window_size=self.window_size)
             aggregation_df = pd.concat(list_of_stat_features, axis=1)
+            aggregation_df = pd.concat([aggregation_df, global_features], axis=1)
+            aggregation_df = aggregation_df.fillna(0)
         else:
-            aggregation_df = self.get_statistical_features(ts)
+            statistical_features = self.get_statistical_features(ts)
+            global_features = self.get_statistical_features(ts, add_global_features=True)
+            aggregation_df = pd.concat([statistical_features, global_features], axis=1)
+            aggregation_df = aggregation_df.fillna(0)
         return aggregation_df
 
     def generate_features_from_ts(self,
@@ -108,11 +103,6 @@ class QuantileExtractor(BaseExtractor):
             mode = 'SingleTS'
         else:
             mode = 'MultiTS'
-
-        # either_result = Either.insert(self.extract_stats_features(ts)) if mode == 'SingleTS' else Either.insert(self.__get_feature_matrix(ts))
-        # either_result = either_result.bind(lambda x: x if x.monoid[1] else Either.insert(self.__component_extraction(ts)))
-        # aggregation_df = either_result.value.T
-
         try:
             if mode == 'MultiTS':
                 aggregation_df = self.__get_feature_matrix(ts)
