7aec5ff1b69c9cf66ba782f3efdbb5757454eb16
vadim_potemkin <aspirantvadim@gmail.com>
2023-09-20 18:30:28 +0200

    added: unit-test and integration, docstrings, init input_data method cleaned up TimeSeriesClassifier_Preset

--- fedot_ind/core/operation/transformation/data/eigen.py
+++ fedot_ind/core/operation/transformation/data/eigen.py
@@ -1,18 +1,37 @@
 import numpy as np
-import pandas as pd
-import copy
+from typing import List, Tuple
 
 
-def weighted_inner_product(F_i, F_j, window_length, ts_length):
-    # Calculate the weights
+def weighted_inner_product(F_i: np.ndarray, F_j: np.ndarray, window_length: int, ts_length: int) -> float:
+    """Calculate the weighted inner product of two vectors.
+
+    Args:
+        F_i: First vector.
+        F_j: Second vector.
+        window_length: Length of the window.
+        ts_length: Total length of the time series.
+
+    Returns:
+        Weighted inner product.
+    """
     first = list(np.arange(window_length) + 1)
-    second = [window_length] * (ts_length - 2*window_length)
+    second = [window_length] * (ts_length - 2 * window_length)
     third = list(np.arange(window_length) + 1)[::-1]
     w = np.array(first + second + third)
-    return w.dot(F_i * F_j)
+    return float(w.dot(F_i * F_j))
 
 
-def calculate_matrix_norms(TS_comps, window_length, ts_length):
+def calculate_matrix_norms(TS_comps: np.ndarray, window_length: int, ts_length: int) -> np.ndarray:
+    """Calculate matrix norms for the time series components.
+
+    Args:
+        TS_comps: The time series components.
+        window_length: Length of the window.
+        ts_length: Total length of the time series.
+
+    Returns:
+        Array of matrix norms.
+    """
     r = []
     for i in range(TS_comps.shape[1]):
         r.append(weighted_inner_product(TS_comps[:, i], TS_comps[:, i], window_length, ts_length))
@@ -21,45 +40,48 @@ def calculate_matrix_norms(TS_comps, window_length, ts_length):
     return F_wnorms
 
 
-def calculate_corr_matrix(TS_comps, F_wnorms, window_length, ts_length):
-    Wcorr = np.identity(TS_comps.shape[1])
+def calculate_corr_matrix(ts_comps: np.ndarray,
+                          f_wnorms: np.ndarray,
+                          window_length: int,
+                          ts_length: int) -> Tuple[np.ndarray, List[int]]:
+    """Calculate the w-correlation matrix for the time series components.
+
+    Args:
+        ts_comps: The time series components.
+        f_wnorms: Matrix norms of the time series components.
+        window_length: Length of the window.
+        ts_length: Total length of the time series.
+
+    Returns:
+        W-correlation matrix and a list of component indices.
+    """
+    Wcorr = np.identity(ts_comps.shape[1])
     for i in range(Wcorr.shape[0]):
         for j in range(i + 1, Wcorr.shape[0]):
             Wcorr[i, j] = abs(
-                weighted_inner_product(TS_comps[:, i], TS_comps[:, j], window_length, ts_length) *
-                F_wnorms[i] * F_wnorms[j])
+                weighted_inner_product(ts_comps[:, i], ts_comps[:, j], window_length, ts_length) *
+                f_wnorms[i] * f_wnorms[j])
             Wcorr[j, i] = Wcorr[i, j]
     return Wcorr, [i for i in range(Wcorr.shape[0])]
 
 
-def combine_eigenvectors(TS_comps, window_length,  correlation_level: float = 0.8):
-    """Calculates the w-correlation matrix for the time series.
+def combine_eigenvectors(ts_comps: np.ndarray, window_length: int) -> List[np.ndarray]:
+    """Combine eigenvectors based on the w-correlation matrix for the time series.
 
     Args:
-        TS_comps (np.ndarray): The time series components.
-        correlation_level (float): threshold value of Pearson correlation, using for merging eigenvectors.
-        ts_length (int): The length of TS .
-        window_length (int): The length of TS window.
+        ts_comps (np.ndarray): The time series components.
+        window_length (int): Length of the window.
 
-
+    Returns:
+        List[np.ndarray]: List of combined eigenvectors.
     """
-    combined_components = []
-    ts_length = TS_comps.shape[0]
-    # Calculated weighted norms
-    F_wnorms = calculate_matrix_norms(TS_comps, window_length, ts_length)
-
-    # Calculate Wcorr.
-    Wcorr, components = calculate_corr_matrix(TS_comps, F_wnorms, window_length, ts_length)
-
+    ts_length = ts_comps.shape[0]
+    F_wnorms = calculate_matrix_norms(ts_comps, window_length, ts_length)
+    Wcorr, components = calculate_corr_matrix(ts_comps, F_wnorms, window_length, ts_length)
     combined_components = []
     current_group = []
     for i in range(len(components)):
-        if i == 0 or Wcorr[i, i-1] > correlation_level:
-            current_group.append(TS_comps[:, i])
-        else:
-            combined_components.append(np.array(current_group).sum(axis=0))
-            current_group = [TS_comps[:, i]]
-
-
-
+        combined_components.append(np.array(current_group).sum(axis=0))
+        current_group = [ts_comps[:, i]]
+    combined_components.append(np.array(current_group).sum(axis=0))
     return combined_components
