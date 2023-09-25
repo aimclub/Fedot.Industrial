50cb0b9f51119e7845f2e76bebcaea8769094aeb
v1docq <ir@raxeltelematics.com>
2023-09-25 17:05:56 +0300

    refactoring examples directory

--- examples/pipeline_example/time_series/ts_regression/multi_ts_example.py
+++ examples/pipeline_example/time_series/ts_regression/multi_ts_example.py
@@ -9,13 +9,10 @@ from fedot.core.repository.dataset_types import DataTypesEnum
 from fedot.core.repository.tasks import Task, TaskTypesEnum
 from fedot.core.pipelines.node import PipelineNode
 from fedot.core.pipelines.pipeline import Pipeline
-from matplotlib import pyplot as plt
 
 from fedot_ind.core.architecture.preprocessing.DatasetLoader import DataLoader
 from fedot_ind.core.models.recurrence.reccurence_extractor import RecurrenceExtractor
 from fedot_ind.core.models.quantile.quantile_extractor import QuantileExtractor
-from fedot_ind.core.operation.transformation.basis.eigen_basis import EigenBasisImplementation
-from fedot_ind.core.operation.transformation.basis.fourier import FourierBasisImplementation
 from sklearn.decomposition import PCA
 from sklearn.metrics import explained_variance_score, max_error, mean_absolute_error, \
     mean_squared_error, d2_absolute_error_score, \
@@ -109,13 +106,6 @@ def evaluate_baseline(train, train_target, test, test_target):
     labels_baseline = baseline_model.predict(input_predict).predict
     metric_df_baseline = calculate_metric(test_target, labels_baseline)
     return metric_df_baseline
-
-    # ddb_features_train = EigenBasisImplementation({'window_size': 30,
-    #                                                     'sv_selector': 'median'}).transform(train_input_data)
-    # fourier_features_train = FourierBasisImplementation({"spectrum_type": "smoothed",
-    #                                                      "threshold": 20000}).transform(train_input_data)
-    # fourier_features_test = FourierBasisImplementation({"spectrum_type": "smoothed",
-    #                                                     "threshold": 20000}).transform(test_input_data)
 
 
 if __name__ == "__main__":
