f9bca5026a5efd48cf377fe2a92e32818110eb26
v1docq <ir@raxeltelematics.com>
2023-09-25 17:05:56 +0300

    refactoring examples directory

---
+++ examples/pipeline_example/time_series/ts_forecasting/ssa_forecasting.py
@@ -0,0 +1,60 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from fedot.core.composer.metrics import smape
+from fedot.core.pipelines.pipeline_builder import PipelineBuilder
+
+from examples.example_utils import get_ts_data
+from fedot_ind.core.repository.initializer_industrial_models import IndustrialModels
+
+
+def plot_metrics_and_prediction(test_data,
+                                train_data,
+                                model_prediction,
+                                baseline_prediction,
+                                model_name,
+                                dataset_name):
+    plt.title(dataset_name)
+    plt.plot(train_data.idx, test_data.features, label='features')
+    plt.plot(test_data.idx, test_data.target, label='target')
+    plt.plot(test_data.idx, model_prediction, label=model_name)
+    plt.plot(test_data.idx, baseline_prediction, label='predicted baseline')
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+    print(f"SSA smape: {smape(test_data.target, model_prediction)}")
+    print(f"no SSA smape: {smape(test_data.target, baseline_prediction)}")
+
+
+model_dict = {'ssa_forecasting': PipelineBuilder().add_node('data_driven_basis_for_forecasting',
+                                                            params={'window_size': 35}
+                                                            ),
+              'baseline': PipelineBuilder().add_node('ar')}
+
+datasets = {
+    'm4_yearly': f'../data/ts/M4YearlyTest.csv',
+    'm4_weekly': f'../data/ts/M4WeeklyTest.csv',
+    'm4_daily': f'../data/ts/M4DailyTest.csv',
+    'm4_monthly': f'../data/ts/M4MonthlyTest.csv',
+    'm4_quarterly': f'../data/ts/M4QuarterlyTest.csv'}
+
+forecast_length = 13
+
+if __name__ == '__main__':
+
+    train_data, test_data, dataset_name = get_ts_data('m4_monthly', forecast_length)
+    baseline = model_dict['baseline'].build()
+    baseline.fit(train_data)
+    baseline_prediction = np.ravel(baseline.predict(test_data).predict)
+    with IndustrialModels():
+        for model in model_dict.keys():
+            pipeline = model_dict[model].build()
+            pipeline.fit(train_data)
+            model_prediction = np.ravel(pipeline.predict(test_data).predict)
+            plot_metrics_and_prediction(test_data,
+                                        train_data,
+                                        model_prediction,
+                                        baseline_prediction,
+                                        model,
+                                        dataset_name)
