1a3f4988e6a9f5e27a54f9fa45ff853e457439c9
valer1435 <valp998@gmail.com>
2024-01-18 13:54:28 +0300

    add Ind mutations

--- fedot_ind/core/repository/initializer_industrial_models.py
+++ fedot_ind/core/repository/initializer_industrial_models.py
@@ -1,14 +1,21 @@
 import pathlib
+from random import sample
 
 from fedot.core.composer.metrics import F1, Accuracy
 from fedot.api.api_utils.api_composer import ApiComposer
 from fedot.api.api_utils.api_params_repository import ApiParamsRepository
 from fedot.core.data.merge.data_merger import ImageDataMerger
 from fedot.core.operations.operation import Operation
+from fedot.core.pipelines.adapters import PipelineAdapter
 from fedot.core.pipelines.tuning.search_space import PipelineSearchSpace
 from fedot.core.pipelines.verification import class_rules
 from fedot.core.repository.operation_types_repository import OperationTypesRepository
+from fedot.core.repository.tasks import Task
+from golem.core.dag.graph_utils import graph_has_cycle
 from golem.core.optimisers.genetic.operators.crossover import Crossover
+from golem.core.optimisers.graph import OptGraph, OptNode
+from golem.core.optimisers.optimization_parameters import GraphRequirements
+from golem.core.optimisers.optimizer import GraphGenerationParams
 
 from fedot_ind.api.utils.path_lib import PROJECT_PATH
 from fedot_ind.core.repository.industrial_implementations.abstract import merge_predicts, preprocess_predicts, \
@@ -16,8 +23,61 @@ from fedot_ind.core.repository.industrial_implementations.abstract import merge_
 from fedot_ind.core.repository.industrial_implementations.metric import metric_f1, metric_acc
 from fedot_ind.core.repository.industrial_implementations.optimisation import _get_default_industrial_mutations, \
     MutationStrengthEnumIndustrial, has_no_data_flow_conflicts_in_industrial_pipeline, _crossover_by_type
+from fedot_ind.core.repository.model_repository import AtomizedModel
 from fedot_ind.core.tuning.search_space import get_industrial_search_space
 
+class IndustrialMutations:
+    def __init__(self, task_type):
+        self.node_adapter = PipelineAdapter()
+        self.task_type = Task(task_type)
+        self.industrial_data_operations = [*list(AtomizedModel.INDUSTRIAL_PREPROC_MODEL.value.keys()),
+                                           *list(AtomizedModel.INDUSTRIAL_CLF_PREPROC_MODEL.value.keys()),
+                                           *list(AtomizedModel.FEDOT_PREPROC_MODEL.value.keys())]
+
+    def transform_to_pipeline_node(self, node):
+        return self.node_adapter._transform_to_pipeline_node(node)
+
+    def transform_to_opt_node(self, node):
+        return self.node_adapter._transform_to_opt_node(node)
+
+    def single_edge_mutation(self,
+                             graph: OptGraph,
+                             requirements: GraphRequirements,
+                             graph_gen_params: GraphGenerationParams,
+                             parameters: 'GPAlgorithmParameters'
+                             ) -> OptGraph:
+        """
+        This mutation adds new edge between two random nodes in graph.
+
+        :param graph: graph to mutate
+        """
+
+        def nodes_not_cycling(source_node: OptNode, target_node: OptNode):
+            parents = source_node.nodes_from
+            while parents:
+                if target_node not in parents:
+                    grandparents = []
+                    for parent in parents:
+                        grandparents.extend(parent.nodes_from)
+                    parents = grandparents
+                else:
+                    return False
+            return True
+
+        for _ in range(parameters.max_num_of_operator_attempts):
+            if len(graph.nodes) < 2 or graph.depth > requirements.max_depth:
+                return graph
+
+            source_node, target_node = sample(graph.nodes, 2)
+            if source_node not in target_node.nodes_from:
+                if graph_has_cycle(graph):
+                    graph.connect_nodes(source_node, target_node)
+                    break
+                else:
+                    if nodes_not_cycling(source_node, target_node):
+                        graph.connect_nodes(source_node, target_node)
+                        break
+        return graph
 
 class IndustrialModels:
     def __init__(self):
