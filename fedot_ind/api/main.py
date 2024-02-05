few new units and minor fixes

fedot_ind/api/main.py
@@ -130,8 +130,8 @@ class FedotIndustrial(Fedot):
     def _predict_raf_ensemble(self):
         self.predict_for_ensemble_branch = [x.predict(self.predict_data).predict for x in self.solver[1:]]
         n_samples, n_channels, n_classes = self.predict_for_ensemble_branch[0].shape[0], \
-                                           len(self.predict_for_ensemble_branch), \
-                                           self.predict_for_ensemble_branch[0].shape[1]
+            len(self.predict_for_ensemble_branch), \
+            self.predict_for_ensemble_branch[0].shape[1]
         input_for_head = np.hstack(self.predict_for_ensemble_branch).reshape(n_samples, n_channels, n_classes)
         self.predict_data.features = input_for_head
         self.predict_for_head_ensemble = self.solver[0].predict(self.predict_data).predict
@@ -366,8 +366,8 @@ class FedotIndustrial(Fedot):
                    'shap': NotImplementedError,
                    'lime': NotImplementedError}
 
-        explainer = methods[kwargs.get('method', 'point')](model=self.solver,
-                                                           features=self.predict_data.features,
+        explainer = methods[kwargs.get('method', 'point')](model=self,
+                                                           features=self.predict_data.features.squeeze(),
                                                            target=self.predict_data.target)
         metric = kwargs.get('metric', 'rmse')
         window = kwargs.get('window', 5)

fedot_ind/tools/explain/explain.py
@@ -40,7 +40,7 @@ class PointExplainer(Explainer):
 
     def explain(self, n_samples: int = 1, window: int = 5, method: str = 'rmse'):
         self.picked_feature, self.picked_target = self.select(self.features,
-                                                              self.target,
+                                                              self.target.flatten(),
                                                               n_samples_=n_samples)
         self.scaled_vector, self.window_length = self.importance(window=window,
                                                                  method=method)
@@ -108,6 +108,8 @@ class PointExplainer(Explainer):
     def select(features_, target_, n_samples_: int = 3):
         selected_df = pd.DataFrame()
         selected_target = np.array([])
+        if not isinstance(features_, pd.DataFrame):
+            features_ = pd.DataFrame(features_)
         df = features_.copy()
         df['target'] = target_
         for class_label in np.unique(target_):

tests/unit/api/main/test_api_main.py
@@ -1,5 +1,8 @@
+import warnings
+
 import numpy as np
 import pytest
+from matplotlib import get_backend, pyplot as plt
 
 from fedot_ind.api.main import FedotIndustrial
 from fedot_ind.tools.synthetic.ts_datasets_generator import TimeSeriesDatasetsGenerator
@@ -63,8 +66,9 @@ def test_fit_predict_classification(fedot_industrial_classification, data):
 
     num_unique = np.unique(data[1])
     predict_proba = fedot_industrial_classification.predict_proba(data)
+    metrics = fedot_industrial_classification.get_metrics(target=data[1])
     assert predict_proba.shape[0] == data[1].shape[0]
-
+    assert metrics is not None
     if len(num_unique) > 2:
         assert predict_proba.shape[1] == len(num_unique)
     else:
@@ -119,3 +123,26 @@ def test_generate_anomaly_ts(fedot_industrial_classification, ts_config, anomaly
         for interval in synth_inters[anomaly_type]:
             ts_range = range(len(init_synth_ts))
             assert interval[0] in ts_range and interval[1] in ts_range
+
+
+def test_finetune(fedot_industrial_classification):
+    industrial = fedot_industrial_classification
+    data = univariate_clf_data()
+    industrial.fit(data)
+    industrial.finetune(data)
+    assert industrial.solver is not None
+
+
+def test_plot_methods(fedot_industrial_classification):
+    industrial = fedot_industrial_classification
+    data = univariate_clf_data()
+    industrial.fit(data)
+    labels = industrial.predict(data)
+    probs = industrial.predict_proba(data)
+
+    # switch to non-Gui, preventing plots being displayed
+    # suppress UserWarning that agg cannot show plots
+    curr_backend = get_backend()
+    plt.switch_backend("Agg")
+    warnings.filterwarnings("ignore", "Matplotlib is currently using agg")
+    fedot_industrial_classification.explain()