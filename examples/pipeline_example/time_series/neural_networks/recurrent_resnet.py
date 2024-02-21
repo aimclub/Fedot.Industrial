71da126b0962891818e85c21f7ea2c7d0becc89a
vadim_potemkin <aspirantvadim@gmail.com>
2024-02-21 13:37:36 +0100

    refactored base_nn_model: best val model saving, new methods

--- examples/pipeline_example/time_series/neural_networks/recurrent_resnet.py
+++ examples/pipeline_example/time_series/neural_networks/recurrent_resnet.py
@@ -1,13 +1,11 @@
-import numpy as np
 from fedot.core.pipelines.pipeline_builder import PipelineBuilder
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 
 from fedot_ind.api.utils.data import init_input_data
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 from fedot_ind.core.repository.initializer_industrial_models import IndustrialModels
 from fedot_ind.tools.loader import DataLoader
 
 if __name__ == "__main__":
-    # dataset_name = 'HandOutlines'
     dataset_name = 'Lightning7'
     train_data, test_data = DataLoader(dataset_name=dataset_name).load_data()
     input_train_data = init_input_data(train_data[0], train_data[1])
@@ -15,7 +13,7 @@ if __name__ == "__main__":
 
     metric_dict = {'accuracy': accuracy_score, 'f1': f1_score, 'roc_auc': roc_auc_score}
     with IndustrialModels():
-        pipeline = PipelineBuilder().add_node('recurrence_extractor', params={'window_size': 15,
+        pipeline = PipelineBuilder().add_node('recurrence_extractor', params={'window_size': 30,
                                                                               'stride': 1,
                                                                               'image_mode': True}) \
             .add_node('resnet_model', params={'epochs': 10}) \
@@ -23,9 +21,6 @@ if __name__ == "__main__":
         pipeline.fit(input_train_data)
         output = pipeline.predict(input_test_data)
 
-        # predict = np.array(output.predict.flatten()) + 1
-
         predict = output.predict.flatten()
-
         accuracy = accuracy_score(test_data[1], predict)
         _ = 1
