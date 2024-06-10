b8ba67dd495aced553abb164459fc93390eb8780
Lopa10ko <justforprojects@mail.ru>
2024-06-10 16:45:04 +0300

    refactor: parametrize unit tests with anomaly detectors

--- tests/unit/models/anomaly_detection_pipelines.py
+++ tests/unit/models/anomaly_detection_pipelines.py
@@ -1,50 +1,20 @@
+import pytest
+
 from fedot_ind.core.architecture.pipelines.abstract_pipeline import AbstractPipeline
 from fedot_ind.core.repository.constanst_repository import VALID_LINEAR_DETECTION_PIPELINE
 
-UNI_MULTI_CLF = [dict(benchmark='valve1',
-                      dataset='1')]
-TASK = 'classification'
-TASK_PARAMS = dict(industrial_strategy='anomaly_detection',
-                   detection_window=10)
+UNI_MULTI_CLF = [dict(benchmark='valve1', dataset='1')]
 
 
-def test_isolation_forest(
-        node_list=VALID_LINEAR_DETECTION_PIPELINE['iforest_detector'],
-        data_list=None):
+@pytest.mark.parametrize('node_list', VALID_LINEAR_DETECTION_PIPELINE.values())
+def test_anomaly_detector(node_list, data_list=None):
     if data_list is None:
         data_list = UNI_MULTI_CLF
+
     result = [
         AbstractPipeline(
-            task=TASK,
-            task_params=TASK_PARAMS).evaluate_pipeline(
-            node_list,
-            data) for data in data_list]
-    assert result is not None
-
-
-def test_arima_fault(
-        node_list=VALID_LINEAR_DETECTION_PIPELINE['arima_detector'],
-        data_list=None):
-    if data_list is None:
-        data_list = UNI_MULTI_CLF
-    result = [
-        AbstractPipeline(
-            task=TASK,
-            task_params=TASK_PARAMS).evaluate_pipeline(
-            node_list,
-            data) for data in data_list]
-    assert result is not None
-
-
-def test_encoder_detector(
-        node_list=VALID_LINEAR_DETECTION_PIPELINE['conv_ae_detector'],
-        data_list=None):
-    if data_list is None:
-        data_list = UNI_MULTI_CLF
-    result = [
-        AbstractPipeline(
-            task=TASK,
-            task_params=TASK_PARAMS).evaluate_pipeline(
-            node_list,
-            data) for data in data_list]
+            task='classification',
+            task_params=dict(industrial_strategy='anomaly_detection', detection_window=10)
+        ).evaluate_pipeline(node_list, data) for data in data_list
+    ]
     assert result is not None
