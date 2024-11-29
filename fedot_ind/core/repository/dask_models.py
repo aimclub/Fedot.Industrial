28207c038b223ff961b4c973c7246764583ddb89
technocreep <aspirantvadim@gmail.com>
2024-11-29 13:05:06 +0100

    Add DaskPCA class

--- fedot_ind/core/repository/dask_models.py
+++ fedot_ind/core/repository/dask_models.py
@@ -1,6 +1,7 @@
-from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 from sklearn.utils.validation import check_X_y, check_array
 from dask_ml.linear_model import LogisticRegression, LinearRegression
+from dask_ml.decomposition import PCA
 import numpy as np
 import dask.array as da
 
@@ -127,3 +128,55 @@ class DaskRidgeRegression(BaseEstimator, ClassifierMixin):
         for key, value in params.items():
             setattr(self, key, value)
         return self
+
+
+class DaskPCA(BaseEstimator, TransformerMixin):
+    def __init__(self, params):
+        self.n_components = params.get('n_components')
+        self.model_ = None
+
+    def fit(self, X):
+        """
+        Fit the model using Dask's PCA.
+        """
+        X = check_array(X, accept_sparse=True, dtype=None)
+        if not isinstance(X, da.Array):
+            X = da.from_array(X)
+
+        self.model_ = PCA(n_components=self.n_components)
+        self.model_.fit(X)
+        return self
+
+    def transform(self, X):
+        """
+        Transform the data using the fitted PCA model.
+        """
+        X = check_array(X, accept_sparse=True, dtype=None)
+        if not isinstance(X, da.Array):
+            X = da.from_array(X)
+        return self.model_.transform(X)
+
+    def get_params(self, deep=True):
+        """
+        Return hyperparameter dictionary for compatibility with GridSearchCV.
+        """
+        return {
+            "n_components": self.n_components,
+        }
+
+    def set_params(self, **params):
+        """
+        Set hyperparameters.
+        """
+        for key, value in params.items():
+            setattr(self, key, value)
+        return self
+
+    def inverse_transform(self, X):
+        """
+        Transform the data back to its original space.
+        """
+        X = check_array(X, accept_sparse=True, dtype=None)
+        if not isinstance(X, da.Array):
+            X = da.from_array(X)
+        return self.model_.inverse_transform(X)
