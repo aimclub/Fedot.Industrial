2c01beb9bb4587e3ec1fe696b57af60de557a934
Lopa10ko <justforprojects@mail.ru>
2025-01-17 18:26:24 +0300

    chore: rewrite pdl_clf bench api_config

--- examples/real_world_examples/benchmark_example/classification/PDL_uni.py
+++ examples/real_world_examples/benchmark_example/classification/PDL_uni.py
@@ -1,24 +1,37 @@
 from benchmark.benchmark_TSC import BenchmarkTSC
+from fedot_ind.core.repository.config_repository import DEFAULT_COMPUTE_CONFIG
+from fedot_ind.tools.serialisation.path_lib import PROJECT_PATH
 
-init_assumption_pdl = ['quantile_extractor', 'pdl_clf']
-init_assumption_rf = ['quantile_extractor', 'rf']
-comparasion_dict = dict(pairwise_approach=init_assumption_pdl,
-                        baseline=init_assumption_rf)
-experiment_setup = {
-    'problem': 'classification',
-    'metric': 'accuracy',
-    'timeout': 2.0,
-    'num_of_generations': 15,
-    'pop_size': 10,
-    'metric_names': ('f1', 'accuracy'),
-    'logging_level': 10,
-    'n_jobs': -1,
-    'initial_assumption': comparasion_dict,
-    'output_folder': r'D:\\WORK\\Repo\\Industiral\\IndustrialTS/benchmark/results/',
-    'finetune': True}
+COMPARISON_DICT = dict(pairwise_approach={0: ['quantile_extractor', 'pdl_clf']},
+                       baseline={0: ['quantile_extractor', 'rf']})
+METRIC_NAMES = ('f1', 'accuracy', 'precision', 'roc_auc')
+
+COMPUTE_CONFIG = DEFAULT_COMPUTE_CONFIG
+COMPUTE_CONFIG['output_folder'] = PROJECT_PATH + '/benchmark/results/'
+AUTOML_CONFIG = {'task': 'classification',
+                 'use_automl': True,
+                 'optimisation_strategy': {'optimisation_strategy': {'mutation_agent': 'bandit',
+                                                                     'mutation_strategy': 'growth_mutation_strategy'},
+                                           'optimisation_agent': 'Industrial'}}
+AUTOML_LEARNING_STRATEGY = dict(timeout=2,
+                                pop_size=10,
+                                n_jobs=-1,
+                                num_of_generations=15)
+
+LEARNING_CONFIG = {'learning_strategy': 'from_scratch',
+                   'learning_strategy_params': AUTOML_LEARNING_STRATEGY,
+                   'optimisation_loss': {'quality_loss': 'accuracy'}}
+
+INDUSTRIAL_CONFIG = {'problem': 'classification'}
+
+API_CONFIG = {'industrial_config': INDUSTRIAL_CONFIG,
+              'automl_config': AUTOML_CONFIG,
+              'learning_config': LEARNING_CONFIG,
+              'compute_config': COMPUTE_CONFIG}
 
 if __name__ == "__main__":
-    benchmark = BenchmarkTSC(experiment_setup=experiment_setup,
-                             use_small_datasets=True)
-    benchmark.run()
-    _ = 1
+    BenchmarkTSC(experiment_setup=API_CONFIG,
+                 use_small_datasets=True,
+                 metric_names=METRIC_NAMES,
+                 initial_assumptions=COMPARISON_DICT,
+                 finetune=True).run()
