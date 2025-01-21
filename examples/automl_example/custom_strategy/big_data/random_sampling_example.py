62bd8d9512ee18775a39514149b26ad2f1839976
ismonnar <ivanmonnar@gmail.com>
2025-01-22 02:49:22 +0300

    adaptated examples for using with new ApiTemplate

--- examples/automl_example/custom_strategy/big_data/random_sampling_example.py
+++ examples/automl_example/custom_strategy/big_data/random_sampling_example.py
@@ -1,5 +1,7 @@
 from examples.automl_example.custom_strategy.big_data.big_dataset_utils import create_big_dataset
 from fedot_ind.core.architecture.pipelines.abstract_pipeline import ApiTemplate
+from fedot_ind.core.repository.config_repository import DEFAULT_COMPUTE_CONFIG, \
+    DEFAULT_AUTOML_LEARNING_CONFIG
 
 cur_params = {'rank': None}
 sampling_algorithm = {'CUR': cur_params}
@@ -8,6 +10,29 @@ sampling_algorithm = {'CUR': cur_params}
 def eval_fedot_on_fold(dataset_name, fold):
     return create_big_dataset(dataset_name, fold)
 
+INDUSTRIAL_PARAMS = {'data_type': 'tensor',
+                     'learning_strategy': 'big_dataset',
+                     'sampling_strategy': sampling_algorithm
+                     }
+
+AUTOML_LEARNING_STRATEGY = DEFAULT_AUTOML_LEARNING_CONFIG
+COMPUTE_CONFIG = DEFAULT_COMPUTE_CONFIG
+AUTOML_CONFIG = {'task': 'classification',
+                 'use_automl': True,
+                 'optimisation_strategy': {'optimisation_strategy': {'mutation_agent': 'bandit',
+                                                                     'mutation_strategy': 'growth_mutation_strategy'},
+                                           'optimisation_agent': 'Industrial'}}
+LEARNING_CONFIG = {'learning_strategy': 'from_scratch',
+                   'learning_strategy_params': AUTOML_LEARNING_STRATEGY,
+                   'optimisation_loss': {'quality_loss': 'f1'}}
+INDUSTRIAL_CONFIG = {'problem': 'classification',
+                     'strategy': 'tabular',
+                     'strategy_params': INDUSTRIAL_PARAMS
+                     }
+API_CONFIG = {'industrial_config': INDUSTRIAL_CONFIG,
+              'automl_config': AUTOML_CONFIG,
+              'learning_config': LEARNING_CONFIG,
+              'compute_config': COMPUTE_CONFIG}
 
 if __name__ == "__main__":
     metric_by_fold = {}
@@ -31,8 +56,8 @@ if __name__ == "__main__":
                       n_jobs=-1,
                       logging_level=20)
     for fold in range(10):
-        dataset_dict = create_big_dataset(dataset_name, fold)
-        result_dict = ApiTemplate(api_config=api_config,
+        dataset_dict = eval_fedot_on_fold(dataset_name, fold)
+        result_dict = ApiTemplate(api_config=API_CONFIG,
                                   metric_list=metric_names).eval(dataset=dataset_dict,
                                                                  finetune=finetune)
         metric_by_fold.update({fold: result_dict})
