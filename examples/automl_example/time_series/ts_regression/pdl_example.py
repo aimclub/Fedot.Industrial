168038db951741ca0f4b46255926152b99098c0a
Lopa10ko <justforprojects@mail.ru>
2025-01-22 17:54:55 +0300

    feat: add pdl regression example

--- examples/automl_example/time_series/ts_regression/pdl_example.py
+++ examples/automl_example/time_series/ts_regression/pdl_example.py
@@ -1,22 +1,25 @@
 from fedot_ind.core.architecture.pipelines.abstract_pipeline import ApiTemplate
+from fedot_ind.core.repository.config_repository import DEFAULT_COMPUTE_CONFIG, DEFAULT_REG_AUTOML_CONFIG, \
+    DEFAULT_AUTOML_LEARNING_CONFIG
+
 
 if __name__ == "__main__":
-    dataset_name = 'AppliancesEnergy'  # BeijingPM10Quality
-    finetune = False
-    api_config = dict(problem='regression',
-                      metric='rmse',
-                      timeout=0.1,
-                      n_jobs=2,
-                      logging_level=20)
-    metric_names = ('r2', 'rmse', 'mae')
-    init_assumption_pdl = ['quantile_extractor', 'pdl_reg']
-    init_assumption_rf = ['quantile_extractor', 'treg']
-    comparasion_dict = dict(pairwise_approach=init_assumption_pdl,
-                            baseline=init_assumption_rf)
-    for approach in comparasion_dict.keys():
-        result_dict = ApiTemplate(api_config=api_config,
-                                  metric_list=metric_names).eval(dataset=dataset_name,
-                                                                 initial_assumption=comparasion_dict[approach],
-                                                                 finetune=finetune)
-        metrics = result_dict['metrics']
-        print(f'Approach - {approach}. Metrics - {metrics}')
+    DATASET_NAME = 'AppliancesEnergy'
+
+    API_CONFIG = {'industrial_config': {'problem': 'regression'},
+                  'automl_config': DEFAULT_REG_AUTOML_CONFIG,
+                  'learning_config': {'learning_strategy': 'from_scratch',
+                                      'learning_strategy_params': DEFAULT_AUTOML_LEARNING_CONFIG,
+                                      'optimisation_loss': {'quality_loss': 'rmse'}},
+                  'compute_config': DEFAULT_COMPUTE_CONFIG}
+
+    METRIC_NAMES = ('r2', 'rmse', 'mae')
+    COMPARISON_DICT = dict(pairwise_approach={0: ['quantile_extractor', 'pdl_reg']},
+                           baseline={0: ['quantile_extractor', 'treg']})
+
+    for approach, node_list in COMPARISON_DICT.items():
+        result_dict = ApiTemplate(api_config=API_CONFIG,
+                                  metric_list=METRIC_NAMES).eval(dataset=DATASET_NAME,
+                                                                 initial_assumption=node_list,
+                                                                 finetune=False)
+        print(f'Approach: {approach}. Metrics: {result_dict["metrics"]}')
