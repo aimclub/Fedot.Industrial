53348b186abd2441bc6df2b0e1ee4bd3542fea72
Lopa10ko <justforprojects@mail.ru>
2025-01-22 18:00:38 +0300

    feat: add ts regression example

--- examples/automl_example/time_series/ts_regression/ts_regression_example.py
+++ examples/automl_example/time_series/ts_regression/ts_regression_example.py
@@ -1,14 +1,18 @@
 from fedot_ind.core.architecture.pipelines.abstract_pipeline import ApiTemplate
+from fedot_ind.core.repository.config_repository import DEFAULT_AUTOML_LEARNING_CONFIG, DEFAULT_REG_AUTOML_CONFIG, \
+    DEFAULT_COMPUTE_CONFIG
 
 if __name__ == "__main__":
-    dataset_name = 'AppliancesEnergy'  # BeijingPM10Quality
-    finetune = False
-    api_config = dict(problem='regression',
-                      metric='rmse',
-                      timeout=1,
-                      n_jobs=2,
-                      logging_level=20)
-    metric_names = ('r2', 'rmse', 'mae')
-    result_dict = ApiTemplate(api_config=api_config,
-                              metric_list=('f1', 'accuracy')).eval(dataset=dataset_name, finetune=finetune)
+    dataset_name = 'AppliancesEnergy'
+    METRIC_NAMES = ('r2', 'rmse', 'mae')
+    API_CONFIG = {'industrial_config': {'problem': 'regression'},
+                  'automl_config': DEFAULT_REG_AUTOML_CONFIG,
+                  'learning_config': {'learning_strategy': 'from_scratch',
+                                      'learning_strategy_params': DEFAULT_AUTOML_LEARNING_CONFIG,
+                                      'optimisation_loss': {'quality_loss': 'rmse'}},
+                  'compute_config': DEFAULT_COMPUTE_CONFIG}
+
+    result_dict = ApiTemplate(api_config=API_CONFIG,
+                              metric_list=METRIC_NAMES).eval(dataset=dataset_name,
+                                                             finetune=False)
     print(result_dict['metrics'])
