5de9d8ea596bdfa85b10d6f497694a41e6ffdcf2
Lopa10ko <justforprojects@mail.ru>
2025-01-29 17:10:50 +0300

    fix: add new api test_checkers_collection

--- tests/unit/api/utils/test_checkers_collection.py
+++ tests/unit/api/utils/test_checkers_collection.py
@@ -4,32 +4,23 @@ from fedot_ind.api.utils.checkers_collections import DataCheck
 from fedot_ind.core.architecture.settings.computational import backend_methods as np
 
 
-def input_data_with_nans():
+def get_corrupted_input_data(fill_value):
     array = np.random.rand(10, 10)
     for i in range(10):
         x = np.random.randint(0, 10)
         y = np.random.randint(0, 10)
-        array[x, y] = np.nan
+        array[x, y] = fill_value
     target = np.random.rand(10)
     return array, target
 
 
-def input_data_with_inf():
-    array = np.random.rand(10, 10)
-    for i in range(10):
-        x = np.random.randint(0, 10)
-        y = np.random.randint(0, 10)
-        array[x, y] = np.inf
-    target = np.random.rand(10)
-    return array, target
-
-
-@pytest.mark.parametrize("input_data",
-                         [input_data_with_inf(),
-                          input_data_with_nans()])
-def test_DataCheck(input_data):
+@pytest.mark.parametrize("input_data", (
+    get_corrupted_input_data(np.nan),
+    get_corrupted_input_data(np.inf)
+), ids=['nan_filled', 'inf_filled'])
+def test_data_check(input_data):
     features, target = input_data
-    data_check = DataCheck(input_data=(
-        features, target), task='classification')
+    data_check = DataCheck(input_data=(features, target),
+                           task='classification')
     clean_data = data_check.check_input_data()
     assert clean_data is not None
