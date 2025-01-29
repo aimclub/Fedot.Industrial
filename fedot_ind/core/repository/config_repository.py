746f725734452510eaa4620987ae24f92db61ffc
Lopa10ko <justforprojects@mail.ru>
2025-01-29 17:12:34 +0300

    feat: add default clf and reg learning configs

--- fedot_ind/core/repository/config_repository.py
+++ fedot_ind/core/repository/config_repository.py
@@ -5,8 +5,8 @@ from os import cpu_count
 class ComputeConfigConstant(Enum):
     DEFAULT_COMPUTE_CONFIG = {'backend': 'cpu',
                               'distributed': dict(processes=False,
-                                                  n_workers=1,
-                                                  threads_per_worker=round(cpu_count() / 2),
+                                                  n_workers=2,
+                                                  threads_per_worker=2,
                                                   memory_limit=0.3
                                                   ),
                               'output_folder': './results',
@@ -21,7 +21,7 @@ class AutomlLearningConfigConstant(Enum):
                                  early_stopping_iterations=10,
                                  early_stopping_timeout=10,
                                  with_tuning=False,
-                                 n_jobs=1)
+                                 n_jobs=-1)
 
 
 class AutomlConfigConstant(Enum):
@@ -35,8 +35,18 @@ class AutomlConfigConstant(Enum):
     DEFAULT_TSF_AUTOML_CONFIG = {'task': 'ts_forecasting', **DEFAULT_SUBCONFIG}
 
 
+class LearningConfigConstant(Enum):
+    DEFAULT_SUBCONFIG = {'learning_strategy': 'from_scratch',
+                         'learning_strategy_params': AutomlLearningConfigConstant.DEFAULT_AUTOML_CONFIG.value}
+    DEFAULT_CLF_LEARNING_CONFIG = {'optimisation_loss': {'quality_loss': 'accuracy'}, **DEFAULT_SUBCONFIG}
+    DEFAULT_REG_LEARNING_CONFIG = {'optimisation_loss': {'quality_loss': 'rmse'}, **DEFAULT_SUBCONFIG}
+
+
 DEFAULT_AUTOML_LEARNING_CONFIG = AutomlLearningConfigConstant.DEFAULT_AUTOML_CONFIG.value
 DEFAULT_COMPUTE_CONFIG = ComputeConfigConstant.DEFAULT_COMPUTE_CONFIG.value
 DEFAULT_CLF_AUTOML_CONFIG = AutomlConfigConstant.DEFAULT_CLF_AUTOML_CONFIG.value
 DEFAULT_REG_AUTOML_CONFIG = AutomlConfigConstant.DEFAULT_REG_AUTOML_CONFIG.value
 DEFAULT_TSF_AUTOML_CONFIG = AutomlConfigConstant.DEFAULT_TSF_AUTOML_CONFIG.value
+
+DEFAULT_CLF_LEARNING_CONFIG = LearningConfigConstant.DEFAULT_CLF_LEARNING_CONFIG.value
+DEFAULT_REG_LEARNING_CONFIG = LearningConfigConstant.DEFAULT_REG_LEARNING_CONFIG.value
