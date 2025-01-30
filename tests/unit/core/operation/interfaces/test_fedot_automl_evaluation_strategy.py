72285a55aa4e566bd4e8b0a1b692ed84eb41f34e
Lopa10ko <justforprojects@mail.ru>
2025-01-30 15:07:02 +0300

    fix: update wavelet basis and automl strategy test

--- tests/unit/core/operation/interfaces/test_fedot_automl_evaluation_strategy.py
+++ tests/unit/core/operation/interfaces/test_fedot_automl_evaluation_strategy.py
@@ -1,68 +1,38 @@
+import numpy as np
 import pytest
+from fedot.core.data.data import InputData
 from fedot.core.operations.operation_parameters import OperationParameters
-from fedot_ind.tools.synthetic.ts_datasets_generator import TimeSeriesDatasetsGenerator
-from fedot_ind.api.utils.data import init_input_data
+from fedot.core.repository.dataset_types import DataTypesEnum
+from fedot.core.repository.tasks import TaskTypesEnum, Task
+
 from fedot_ind.core.operation.interfaces.fedot_automl_evaluation_strategy import FedotAutoMLClassificationStrategy, \
     FedotAutoMLRegressionStrategy
+from tests.unit.api.fixtures import get_data_by_task
 
 
-@pytest.fixture()
-def classification_data():
-    generator = TimeSeriesDatasetsGenerator(task='classification',
-                                            binary=True,
-                                            multivariate=False)
-    train_data, _ = generator.generate_data()
+@pytest.mark.parametrize('task', ('classification', 'regression'))
+def test_fedot_automl_strategy_fit_predict(task):
+    (x_train, y_train), _ = get_data_by_task(task)
+    x_train, y_train = x_train.values, y_train
+    input_data = InputData(idx=np.arange(len(x_train)),
+                           features=x_train,
+                           target=y_train,
+                           task=Task(TaskTypesEnum(task)),
+                           data_type=DataTypesEnum.table)
 
-    return init_input_data(
-        train_data[0],
-        train_data[1],
-        task='classification')
+    params = OperationParameters(problem=task, timeout=0.1, n_jobs=1)
+    match task:
+        case 'classification':
+            strategy = FedotAutoMLClassificationStrategy(operation_type='fedot_cls', params=params)
+        case 'regression':
+            strategy = FedotAutoMLRegressionStrategy(operation_type='fedot_regr', params=params)
+        case _:
+            return
 
+    trained_operation = strategy.fit(input_data)
 
-@pytest.fixture()
-def regression_data():
-    generator = TimeSeriesDatasetsGenerator(task='regression',
-                                            binary=True,
-                                            multivariate=False)
-    train_data, _ = generator.generate_data()
-
-    return init_input_data(
-        train_data[0],
-        train_data[1].reshape(-1, 1),
-        task='regression')
-
-
-def fedot_automl_classification_strategy_fit(classification_data):
-    operation_type = 'fedot_cls'
-    params = OperationParameters()
-    params._parameters.update(
-        {'problem': 'classification', 'timeout': 0.1, 'n_jobs': 1})
-    strategy = FedotAutoMLClassificationStrategy(operation_type=operation_type,
-                                                 params=params)
-    trained_operation = strategy.fit(classification_data)
-
-    predict = strategy.predict(trained_operation, classification_data)
-    predict_for_fit = strategy.predict_for_fit(
-        trained_operation, classification_data)
-
-    assert predict.predict is not None
-    assert predict_for_fit.predict is not None
-    assert strategy.operation_impl is not None
-    assert trained_operation is not None
-
-
-def fedot_automl_regression_strategy_fit(regression_data):
-    operation_type = 'fedot_regr'
-    params = OperationParameters()
-    params._parameters.update(
-        {'problem': 'regression', 'timeout': 0.1, 'n_jobs': 1})
-    strategy = FedotAutoMLRegressionStrategy(operation_type=operation_type,
-                                             params=params)
-    trained_operation = strategy.fit(regression_data)
-
-    predict = strategy.predict(trained_operation, regression_data)
-    predict_for_fit = strategy.predict_for_fit(
-        trained_operation, regression_data)
+    predict = strategy.predict(trained_operation, input_data)
+    predict_for_fit = strategy.predict_for_fit(trained_operation, input_data)
 
     assert predict.predict is not None
     assert predict_for_fit.predict is not None
