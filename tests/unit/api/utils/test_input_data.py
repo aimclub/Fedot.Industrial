191a183c5c8c6dd3d9c004bffc9fbd3356872c2c
Lopa10ko <justforprojects@mail.ru>
2025-01-30 15:20:37 +0300

    fix: bring back init_input_data test

--- tests/unit/api/utils/test_input_data.py
+++ tests/unit/api/utils/test_input_data.py
@@ -3,7 +3,7 @@ import pytest
 import numpy as np
 import pandas as pd
 
-from fedot_ind.api.utils.data import init_input_data
+from fedot_ind.core.operation.dummy.dummy_operation import init_input_data
 
 
 @pytest.fixture
