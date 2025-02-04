f649eae028e59273a27036190d49389cefacd0a9
Lopa10ko <justforprojects@mail.ru>
2025-02-05 00:57:18 +0300

    chore: go for more straight-forward repo swap instead of manager

--- tests/unit/core/operation/interfaces/test_fedot_automl_evaluation_strategy.py
+++ tests/unit/core/operation/interfaces/test_fedot_automl_evaluation_strategy.py
@@ -14,32 +14,30 @@ from tests.unit.api.fixtures import get_data_by_task
 @pytest.mark.parametrize('task', ('classification', 'regression'))
 def test_fedot_automl_strategy_fit_predict(task):
     repo = IndustrialModels()
-    try:
-        repo.setup_default_repository()
-        (x_train, y_train), _ = get_data_by_task(task)
-        x_train, y_train = x_train.values, y_train
-        input_data = InputData(idx=np.arange(len(x_train)),
-                               features=x_train,
-                               target=y_train,
-                               task=Task(TaskTypesEnum(task)),
-                               data_type=DataTypesEnum.table)
+    repo.setup_default_repository()
 
-        params = OperationParameters(problem=task, timeout=0.1, n_jobs=1)
-        if task == 'classification':
-            strategy = FedotAutoMLClassificationStrategy(operation_type='fedot_cls', params=params)
-        elif task == 'regression':
-            strategy = FedotAutoMLRegressionStrategy(operation_type='fedot_regr', params=params)
-        else:
-            return
+    (x_train, y_train), _ = get_data_by_task(task)
+    x_train, y_train = x_train.values, y_train
+    input_data = InputData(idx=np.arange(len(x_train)),
+                           features=x_train,
+                           target=y_train,
+                           task=Task(TaskTypesEnum(task)),
+                           data_type=DataTypesEnum.table)
 
-        trained_operation = strategy.fit(input_data)
+    params = OperationParameters(problem=task, timeout=0.1, n_jobs=1)
+    if task == 'classification':
+        strategy = FedotAutoMLClassificationStrategy(operation_type='fedot_cls', params=params)
+    elif task == 'regression':
+        strategy = FedotAutoMLRegressionStrategy(operation_type='fedot_regr', params=params)
+    else:
+        return
 
-        predict = strategy.predict(trained_operation, input_data)
-        predict_for_fit = strategy.predict_for_fit(trained_operation, input_data)
+    trained_operation = strategy.fit(input_data)
 
-        repo.setup_repository()
-    except Exception as ex:
-        raise ex
+    predict = strategy.predict(trained_operation, input_data)
+    predict_for_fit = strategy.predict_for_fit(trained_operation, input_data)
+
+    repo.setup_repository()
 
     assert predict.predict is not None
     assert predict_for_fit.predict is not None
