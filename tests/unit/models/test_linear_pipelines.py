e17a2e03c2cc79eb6c4b4cb30814be7e6db62245
technocreep <aspirantvadim@gmail.com>
2025-02-26 14:53:03 +0100

    included topo in clf and reg unit tests

--- tests/unit/models/test_linear_pipelines.py
+++ tests/unit/models/test_linear_pipelines.py
@@ -94,9 +94,10 @@ LINEAR_DETECTION_PIPELINE_CASES = [
 ]
 
 # TODO: temporarily workaround skip topological_*
-BANNED_LINEAR_PIPELINE_LABELS = ['topological_clf',
-                                 'topological_reg',
-                                 'composite_reg',
+BANNED_LINEAR_PIPELINE_LABELS = [
+    # 'topological_clf',
+                                #  'topological_reg',
+                                #  'composite_reg',
                                  'topological_lgbm',
                                  'composite_clf',
                                  'stat_detector',
