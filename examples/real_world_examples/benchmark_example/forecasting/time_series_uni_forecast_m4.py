9e9ea2c5290ae1d3da64a2b874aedf163515f294
Lopa10ko <justforprojects@mail.ru>
2025-02-28 19:20:16 +0300

    update regression SOTA_multi and forecasting M4_uni bench examples

--- examples/real_world_examples/benchmark_example/forecasting/time_series_uni_forecast_m4.py
+++ examples/real_world_examples/benchmark_example/forecasting/time_series_uni_forecast_m4.py
@@ -1,49 +1,33 @@
-import os
-
 from fedot_ind.api.utils.checkers_collections import ApiConfigCheck
 from fedot_ind.core.architecture.pipelines.abstract_pipeline import ApiTemplate
-from fedot_ind.core.repository.config_repository import DEFAULT_COMPUTE_CONFIG, DEFAULT_TSF_AUTOML_CONFIG, \
-    DEFAULT_TSF_LEARNING_CONFIG, DEFAULT_TSF_INDUSTRIAL_CONFIG
+from fedot_ind.core.repository.config_repository import DEFAULT_TSF_API_CONFIG
 from fedot_ind.core.repository.constanst_repository import M4_FORECASTING_BENCH, M4_FORECASTING_LENGTH
 
 
-def get_benchmark_setup():
-    DEEPAR_LEARNING_PARAMS = {'epochs': 150, 'lr': 0.001, 'device': 'cpu'}
-    model_to_compare = [{0: ['lagged_forecaster']}, {}, {0: [('deepar_model', DEEPAR_LEARNING_PARAMS)]}, {0: ['ar']}]
+def get_m4_model_to_compare():
+    model_to_compare = [{0: ['lagged_forecaster']},
+                        {},
+                        {0: [('deepar_model', {'epochs': 150, 'lr': 0.001, 'device': 'cpu'})]},
+                        {0: ['ar']}]
     model_name = ['lagged_regression', 'industrial', 'deepar', 'ar']
-    finutune_existed_model = [True, False, True, True]
-    BENCHMARK = 'M4'
-    BENCHMARK_PARAMS = {'experiment_date': '24_01_25',
-                        'metadata': M4_FORECASTING_LENGTH,
-                        'datasets': M4_FORECASTING_BENCH,
-                        'model_to_compare': (model_to_compare, model_name, finutune_existed_model)}
-    return BENCHMARK, BENCHMARK_PARAMS
+    finetune_existed_model = [True, False, True, True]
+    return model_to_compare, model_name, finetune_existed_model
 
-
-API_CONFIG = {'industrial_config': DEFAULT_TSF_INDUSTRIAL_CONFIG,
-              'automl_config': DEFAULT_TSF_AUTOML_CONFIG,
-              'learning_config': DEFAULT_TSF_LEARNING_CONFIG,
-              'compute_config': DEFAULT_COMPUTE_CONFIG}
-
-BENCHMARK_CONFIG = {'task': 'ts_forecasting',
-                    'task_params': {'forecast_length': 14},
-                    'timeout': 5,
-                    'n_workers': 2,
-                    'threads_per_worker': 2,
-                    'with_tuning': False,
-                    'logging_level': 20
-                    }
-config_agent = ApiConfigCheck()
 
 if __name__ == "__main__":
-    UPD_API_CONFIG = config_agent.update_config_with_kwargs(config_to_update=API_CONFIG,
-                                                            **BENCHMARK_CONFIG)
+    BENCHMARK_CONFIG = {'task': 'ts_forecasting',
+                        'task_params': {'forecast_length': 14},
+                        'timeout': 5,
+                        'n_workers': 2,
+                        'threads_per_worker': 2,
+                        'with_tuning': False,
+                        'logging_level': 20}
 
-    config_agent.compare_configs(API_CONFIG, UPD_API_CONFIG)
+    UPD_API_CONFIG = ApiConfigCheck().update_config_with_kwargs(DEFAULT_TSF_API_CONFIG, **BENCHMARK_CONFIG)
     api_agent = ApiTemplate(api_config=UPD_API_CONFIG, metric_list=('rmse', 'mae'))
-    BENCHMARK, BENCHMARK_PARAMS = get_benchmark_setup()
-    EVALUATED = os.listdir('./M4_24_01_25/ar')
+    EVALUATED = []
     DATASETS = [x for x in M4_FORECASTING_BENCH if x not in EVALUATED]
-    BENCHMARK_PARAMS['datasets'] = DATASETS
-    api_agent.evaluate_benchmark(benchmark_name=BENCHMARK,
-                                 benchmark_params=BENCHMARK_PARAMS)
+    api_agent.evaluate_benchmark(benchmark_name='M4',
+                                 benchmark_params={'metadata': M4_FORECASTING_LENGTH,
+                                                   'datasets': DATASETS,
+                                                   'model_to_compare': get_m4_model_to_compare()})
--- examples/real_world_examples/benchmark_example/regression/SOTA_multi.py
+++ examples/real_world_examples/benchmark_example/regression/SOTA_multi.py
@@ -2,22 +2,17 @@ import gc
 
 from fedot_ind.api.utils.checkers_collections import ApiConfigCheck
 from fedot_ind.core.architecture.pipelines.abstract_pipeline import ApiTemplate
-from fedot_ind.core.repository.config_repository import (DEFAULT_COMPUTE_CONFIG, DEFAULT_REG_INDUSTRIAL_CONFIG,
-                                                         DEFAULT_REG_AUTOML_CONFIG, DEFAULT_REG_LEARNING_CONFIG)
+from fedot_ind.core.repository.config_repository import DEFAULT_REG_API_CONFIG
 from fedot_ind.core.repository.constanst_repository import MULTI_REG_BENCH
-
-gc.collect()
-
-bagging_ensemble = {0: ['industrial_freq_reg'],
-                    1: ['industrial_stat_reg'],
-                    'head': ['bagging']}
 
 
 def get_benchmark_setup():
     RESNET_LEARNING_PARAMS = {'epochs': 150,
                               'lr': 0.001,
-                              'device': 'cpu'
-                              }
+                              'device': 'cpu'}
+    bagging_ensemble = {0: ['industrial_freq_reg'],
+                        1: ['industrial_stat_reg'],
+                        'head': ['bagging']}
 
     model_to_compare = [bagging_ensemble,
                         {},
@@ -25,30 +20,20 @@ def get_benchmark_setup():
     model_name = ['stat_reg', 'industrial', 'resnet']
     finutune_existed_model = [True, False, True]
     BENCHMARK = 'UCR_REG'
-    BENCHMARK_PARAMS = {'experiment_date': '18_02_25',
-                        'datasets': MULTI_REG_BENCH,
+    BENCHMARK_PARAMS = {'datasets': MULTI_REG_BENCH,
                         'model_to_compare': (model_to_compare, model_name, finutune_existed_model)}
     return BENCHMARK, BENCHMARK_PARAMS
 
 
-API_CONFIG = {'industrial_config': DEFAULT_REG_INDUSTRIAL_CONFIG,
-              'automl_config': DEFAULT_REG_AUTOML_CONFIG,
-              'learning_config': DEFAULT_REG_LEARNING_CONFIG,
-              'compute_config': DEFAULT_COMPUTE_CONFIG}
-
-BENCHMARK_CONFIG = {'task': 'regression',
-                    'timeout': 15,
-                    'n_workers': 2,
-                    'threads_per_worker': 4,
-                    'with_tuning': False,
-                    'logging_level': 20
-                    }
-config_agent = ApiConfigCheck()
-
 if __name__ == "__main__":
-    UPD_API_CONFIG = config_agent.update_config_with_kwargs(config_to_update=API_CONFIG,
-                                                            **BENCHMARK_CONFIG)
-    config_agent.compare_configs(API_CONFIG, UPD_API_CONFIG)
+    gc.collect()
+    BENCHMARK_CONFIG = {'task': 'regression',
+                        'timeout': 3,
+                        'n_workers': 2,
+                        'threads_per_worker': 4,
+                        'with_tuning': False,
+                        'logging_level': 20}
+    UPD_API_CONFIG = ApiConfigCheck().update_config_with_kwargs(DEFAULT_REG_API_CONFIG, **BENCHMARK_CONFIG)
     api_agent = ApiTemplate(api_config=UPD_API_CONFIG, metric_list=('rmse', 'mae'))
     BENCHMARK, BENCHMARK_PARAMS = get_benchmark_setup()
     api_agent.evaluate_benchmark(benchmark_name=BENCHMARK,
