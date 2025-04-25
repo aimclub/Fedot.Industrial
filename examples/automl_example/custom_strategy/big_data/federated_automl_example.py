ec981565a01b88d3f260a2d10b1c23335289c499
Lopa10ko <justforprojects@mail.ru>
2025-04-25 16:20:35 +0300

    feat: add examples as functions

--- examples/automl_example/custom_strategy/big_data/federated_automl_example.py
+++ examples/automl_example/custom_strategy/big_data/federated_automl_example.py
@@ -1,21 +1,22 @@
+from fedot_ind.api.utils.checkers_collections import ApiConfigCheck
 from fedot_ind.core.architecture.pipelines.abstract_pipeline import ApiTemplate
 from fedot_ind.core.repository.config_repository import DEFAULT_COMPUTE_CONFIG, DEFAULT_AUTOML_LEARNING_CONFIG, \
-    DEFAULT_CLF_AUTOML_CONFIG
+    DEFAULT_CLF_AUTOML_CONFIG, DEFAULT_CLF_API_CONFIG
 from fedot_ind.tools.synthetic.ts_datasets_generator import TimeSeriesDatasetsGenerator
 
 
-if __name__ == '__main__':
-    INDUSTRIAL_CONFIG = {'problem': 'classification',
+def run_federated_automl_example(timeout: int = 10):
+    industrial_config = {'problem': 'classification',
                          'data_type': 'time_series',
                          'learning_strategy': 'federated_automl'}
 
-    LEARNING_CONFIG = {'learning_strategy': 'from_scratch',
-                       'learning_strategy_params': DEFAULT_AUTOML_LEARNING_CONFIG,
+    learning_config = {'learning_strategy': 'from_scratch',
+                       'learning_strategy_params': {**DEFAULT_AUTOML_LEARNING_CONFIG, 'timeout': timeout},
                        'optimisation_loss': {'quality_loss': 'f1'}}
 
-    API_CONFIG = {'industrial_config': INDUSTRIAL_CONFIG,
+    api_config = {'industrial_config': industrial_config,
                   'automl_config': DEFAULT_CLF_AUTOML_CONFIG,
-                  'learning_config': LEARNING_CONFIG,
+                  'learning_config': learning_config,
                   'compute_config': DEFAULT_COMPUTE_CONFIG}
 
     train_data, test_data = TimeSeriesDatasetsGenerator(num_samples=1800,
@@ -25,7 +26,12 @@ if __name__ == '__main__':
                                                         test_size=0.5,
                                                         multivariate=False).generate_data()
     dataset_dict = dict(train_data=train_data, test_data=test_data)
-    result_dict = ApiTemplate(api_config=API_CONFIG,
+    result_dict = ApiTemplate(api_config=api_config,
                               metric_list=('f1', 'accuracy')).eval(dataset=dataset_dict,
                                                                    finetune=False)
-    print(result_dict)
+    return result_dict
+
+
+if __name__ == '__main__':
+    result = run_federated_automl_example(timeout=2)
+    print(result)
